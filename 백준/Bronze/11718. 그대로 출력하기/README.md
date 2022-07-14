59c2019c95a7fd3e7b2ea0e5913890b67af20918
yongbeomkwak <48616183+yongbeomkwak@users.noreply.github.com>
2022-07-14 23:50:04 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 그대로 출력하기 - 11718 
+
+[문제 링크](https://www.acmicpc.net/problem/11718) 
+
+### 성능 요약
+
+메모리: 2024 KB, 시간: 4 ms
+
+### 분류
+
+구현(implementation), 문자열(string)
+
---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    string str;
+    while (true){
+        getline(cin, str);
+        if (str=="")
+            break;
+        cout << str << endl;
+    } 
+    return 0;
+}
