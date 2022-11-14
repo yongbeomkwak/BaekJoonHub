1df3e9250252e8fb5f6c740d134193fa8601a5fe
yongbeom Kwak <48616183+yongbeomkwak@users.noreply.github.com>
2022-11-14 22:14:18 +0900

    [level 2] Title: 할인 행사, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,24 @@
+from collections import *
+def solution(want, number, discount):
+    answer = 0
+    check = {}
+    for w, n in zip(want, number):
+        check[w] = n
+    
+    for i in range(len(discount)-9):
+        c = Counter(discount[i:i+10])
+        if c == check:
+            answer += 1
+
+    return answer
+    
+        
+        
+    
+
+        
+    
+    
+    
+    
+    return answer
