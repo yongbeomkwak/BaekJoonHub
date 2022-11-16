0a0737eac092a3bd9b476858f19b44ce5a594b39
yongbeom Kwak <48616183+yongbeomkwak@users.noreply.github.com>
2022-11-16 13:56:38 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 43.40 ms, Memory: 19.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -0,0 +1,32 @@
+from collections import * 
+def solution(queue1, queue2):
+    answer = 0
+    queue1 = deque(queue1)
+    queue2 = deque(queue2)
+    limit = (len(queue1)) * 4
+    tot1 = sum(queue1)
+    tot2 = sum(queue2)
+    total = tot1 + tot2
+
+    if total % 2 != 0:
+        return -1
+
+    while True:
+        if tot1 > tot2:
+            target = queue1.popleft()
+            queue2.append(target)
+            tot1 -= target
+            tot2 += target
+            answer += 1
+        elif tot1 < tot2:
+            target = queue2.popleft()
+            queue1.append(target)
+            tot1 += target
+            tot2 -= target
+            answer += 1
+        else:
+            break
+        if answer == limit:
+            answer = -1
+            break
+    return answer
