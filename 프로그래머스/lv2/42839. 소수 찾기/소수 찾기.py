357b4c48994324c3af678284afcdd7433a1d791f
yongbeom Kwak <48616183+yongbeomkwak@users.noreply.github.com>
2022-11-19 15:56:50 +0900

    [level 2] Title: 소수 찾기, Time: 7.60 ms, Memory: 13.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,29 +1,13 @@
-from itertools import *
-from math import *
-def isPrime(n:int) -> bool:
-    end = int(sqrt(n))
-    if(n == 0 or n == 1):
-        return False
-    elif (n== 2 or n ==3):
-        return True
+from itertools import permutations
+def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+        #모든 경우의 수를 합집합 하고 ,1~n길이까지 
+    a -= set(range(0, 2)) # 0~2 까찌  차집합 하고
     
-    for i in range(2,end+1):
-        if(n%i == 0):
-            return False
-    
-    return True
-
-def solution(numbers):
-    
-    l = len(numbers)
- 
-
-    tmp = []
-    for i in range(1,l+1):
-        p = permutations(list(numbers),i)
-        for n in p:
-            n = int(''.join(n))
-            if(isPrime(n)):
-                tmp.append(n)
-    
-    return len(set(tmp))
+    # 에라토스테네스의 체 진행
+    # 2~ a최댓값의 제곱근 까지 ,
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i) ) # i의 배수를 모두  차집합
+    return len(a)
