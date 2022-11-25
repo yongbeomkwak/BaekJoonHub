35a44f7ab65c884d5deb4500f70687bcb6a176fd
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2022-11-26 00:25:12 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.04 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,42 @@
+from collections import * 
+def solution(maps):
+    n = len(maps) 
+    m = len(maps[0]) 
+    visit = [[False for _ in range(m)] for _ in range(n)]
+    record = [[0 for _ in range(m)] for _ in range(n)]
+    
+    n -= 1
+    m -= 1
+    dx = [0,0,1,-1]
+    dy = [1,-1,0,0]
+    
+    record[0][0] = 1
+    visit[0][0] = True
+    q = deque()
+    q.append((0,0))
+    
+    while q:
+        nowY,nowX = q.popleft()
+        
+        for i in range(4):
+            nextY = nowY + dy[i]
+            nextX = nowX + dx[i]
+            
+            if(nextY>n or nextX >m or nextY<0 or nextX <0):
+                continue
+            
+            if(maps[nextY][nextX] == 0):
+                continue
+            if(visit[nextY][nextX]):
+                continue
+            
+            record[nextY][nextX] = record[nowY][nowX] +1 
+            visit[nextY][nextX] = True
+            q.append((nextY,nextX))
+            
+        
+    
+    if(not(visit[n][m])):
+        return -1 
+    
+    return record[n][m]
