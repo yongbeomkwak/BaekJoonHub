4526c1b271b495fc49125069ae93a0b5328bdcf2
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2022-12-14 21:07:52 +0900

    Update 대문자와 소문자.swift

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.swift
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.swift
@@ -2,5 +2,5 @@ import Foundation
 
 func solution(_ my_string:String) -> String {
    
-    return (my_string.compactMap{ $0.isLowercase ? $0.uppercased() : $0.lowercased() }).joined()
+    return (my_string.compactMap{ $0.isLowercase ? $0.uppercased() : $0.lowercased() }).joined(separator:"")
 }
