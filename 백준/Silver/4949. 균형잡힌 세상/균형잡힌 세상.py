6cfa23fb1ad2d1559d6d322f60883d4f1cb36cab
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-04-01 10:33:42 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 112 ms, Memory: 34348 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -1,56 +1,52 @@
 from sys import *
 from collections import *
 import heapq
-if __name__ =='__main__':
+
+
+def isPair(s1:str,s2:str) -> bool :
     
+    if s1 == "(" and s2 == ")":
+        return True
     
-    while(True):
-        s=stdin.readline()[:-1]
-        if(s=="."):
-            break
-        stack=[]
-        flag:bool=True
-        for c in s:
-            #print(f'{c} {stack}')
-            if(len(stack)==0):
-                if(c=='('or c=='['):
-                    stack.append(c)
-                elif(c==')'or c==']'):
-                    flag=False
-                    break
-                
-            else:
-                if(stack[-1]=='('):
-                    if(c=='(' or c=='['):
-                        stack.append(c)
-                    elif(c==')'):
-                        stack.pop()
-                    elif(c==']'):
-                        flag=False
-                        break
-                elif(stack[-1]=='['):
-                    if(c=='(' or c=='['):
-                        stack.append(c)
-                    elif(c==']'):
-                        stack.pop()
-                    elif(c==')'):
-                        flag=False
-                        break
-            
-                    
-        #print(f'Len: {len(stack)}')
-        if(len(stack)!=0):
-            flag=False
+    if s1 == "[" and s2 == "]":
+        return True
+    
+    return False
+    
+
+
+while True:
+    input = stdin.readline()[:-1]
+    if input == ".":
+        break
+    
+    stack = []
+    for s in input:
+     
+        if s == "(" or s =="[":
+            stack.append(s)
+            continue
         
-        if(flag):
-            stdout.write('yes\n')
-        else:
-            stdout.write('no\n')
+        if s == "]" or s == ")":
+            if not(stack):
+                stack.append(s)
+                break
+            
+            if stack and isPair(stack[-1],s):
+                stack.pop()
+                continue
+            
+            stack.append(s)
+                
+    
+        
+            
+    
+    if stack :
+        stdout.write('no\n')
+    else:
+        stdout.write('yes\n')
+
 
     
     
-    
- 
-   
-    
-   
