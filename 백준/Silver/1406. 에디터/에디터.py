82b2781be30b750c6093ad4761c066bebe959d47
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-04-01 14:46:05 +0900

    Update 에디터.py

--- 백준/Silver/1406. 에디터/에디터.py
+++ 백준/Silver/1406. 에디터/에디터.py
@@ -12,25 +12,28 @@ n = int(stdin.readline()[:-1])
 
 for _ in range(n):
   oper = stdin.readline()[:-1].split()
-    
-   
-    
-  if oper[0] == "L" and prefix :
+
+  if oper[0] == "L" and prefix : 
+      # prefix 존재하고 L일 경우
+      # suffix 쪽으로 옮김
       suffix.append(prefix.pop())
 
   elif oper[0] == "D" and suffix :
-      prefix.append(suffix[-1])
-      suffix.pop()
+      # suffix 의 가장 뒤를 prefix 끝으로 옮김
+      prefix.append(suffix.pop())
+      
 
   elif oper[0] == "B" and prefix:
+    # prefix 있을경우 제거 
     prefix.pop()
 
   elif oper[0] == "P":
+    #prefix 뒤에 붙힘
     c = oper[1]
     prefix.append(c)
 
 
-
+# suffix를 거꾸로 추가
 while suffix:
   prefix.append(suffix.pop())
 
