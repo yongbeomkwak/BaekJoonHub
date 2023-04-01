970f19118e0061059c3aa7f48d8300b374bfe110
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-04-01 10:35:31 +0900

    Update 균형잡힌 세상.py

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -23,25 +23,22 @@ while True:
     stack = []
     for s in input:
      
-        if s == "(" or s =="[":
+        if s == "(" or s =="[": # 열리는 괄호면 담는다.
             stack.append(s)
             continue
         
-        if s == "]" or s == ")":
-            if not(stack):
+        if s == "]" or s == ")": # 닫히는 괄호 일 때 
+            if not(stack): # 스택이 빈 상태에 닫힌괄호가 먼저 나올 경우 잘못된 경우 ,반복문 종료
                 stack.append(s)
                 break
             
-            if stack and isPair(stack[-1],s):
+            if stack and isPair(stack[-1],s): # Piar면 쌍 제거
                 stack.pop()
                 continue
             
-            stack.append(s)
+            stack.append(s) # 아닐경우 삽입
                 
-    
-        
-            
-    
+
     if stack :
         stdout.write('no\n')
     else:
