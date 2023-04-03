7b6e46249f98382a4409706770c3da9e27c6b2ab
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-04-03 20:14:17 +0900

    [Gold IV] Title: 오큰수, Time: 1172 ms, Memory: 154912 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -12,10 +12,10 @@ ans = [-1 for _ in range(n)]
 stack = [0] # 처리가 필요한 인덱스들
 
 for i in range(1, n):
-    while stack and input[stack[-1]] < input[i]:
+    while stack and input[stack[-1]] < input[i]:  # 최근 삽입 인덱스의 오큰수가 발견될 때 마다 , 담겨있는 index에 대한 검사 순회
         ans[stack.pop()] = input[i]
     stack.append(i)
 
 
-print(*ans)
+print(*ans) # 리스트를 간단한 공백과 함께 출력 , * = 압축해제 연산
  
