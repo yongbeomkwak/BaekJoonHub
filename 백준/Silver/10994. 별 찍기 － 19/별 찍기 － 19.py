565c5f6d9fda574d96511f0ff669cd8476185e67
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-04-22 19:17:18 +0900

    [Silver IV] Title: 별 찍기 - 19, Time: 48 ms, Memory: 31648 KB -BaekjoonHub

---
+++ 백준/Silver/10994. 별 찍기 － 19/별 찍기 － 19.py
@@ -0,0 +1,39 @@
+
+
+from sys import *
+
+
+n = int(input())
+
+arr_len = 4 * n - 3
+
+board = [[" "] * arr_len for _ in range(arr_len)]
+
+
+def draw_start(n:int,x:int,y:int):
+    
+    if n == 1:
+        board[y][x] = "*"
+        return
+    
+    curr_len = 4 * n - 3
+    
+    
+    for i in range(curr_len):
+        board[y][x+i] = "*" # 왼쪽 위에서 오른쪽
+        board[y+i][x] = "*" # 왼쪽 위에서 아래로
+        board[y + curr_len - 1][x + i] ="*" # 왼쪽 아래에서 오른쪽으로
+        board[y + i][x + curr_len -1] = "*" # 오른쪽 위에서 아래로 
+        
+    
+    draw_start(n-1,x+2,y+2) # 시작점 우하단으로 2 옮김
+        
+    
+    
+
+
+draw_start(n,0,0)
+
+for star in board:
+    print("".join(star))
+    
