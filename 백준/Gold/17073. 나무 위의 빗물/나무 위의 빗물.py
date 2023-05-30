ebdf208d0c9de29a839f32e2388a46d2785207dc
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-05-30 21:01:25 +0900

    [Gold V] Title: 나무 위의 빗물, Time: 808 ms, Memory: 73484 KB -BaekjoonHub

--- 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
+++ 백준/Gold/17073. 나무 위의 빗물/나무 위의 빗물.py
@@ -8,15 +8,17 @@ def main():
 
     n, w = map(int, input().split())
     degrees = defaultdict(int)
+    leaf = n - 1
     for _ in range(n - 1):
         u, v = map(int, input().split())
         degrees[u] += 1
         degrees[v] += 1
 
-    leaf = 0
-    for i in range(2, n + 1):
-        if degrees[i] == 1:
-            leaf += 1
+        if u != 1 and degrees[u] == 2:
+            leaf -= 1
+        if v != 1 and degrees[v] == 2:
+            leaf -= 1
+
     print(w / leaf)
 
 
