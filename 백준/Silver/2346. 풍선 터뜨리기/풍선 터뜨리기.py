f160f4c069d30d43cf3fd48068ba100a7d3e34c1
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-06-21 12:31:41 +0900

    Update 풍선 터뜨리기.py

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -21,10 +21,13 @@ for _ in range(n):
     ans.append(index)
     dir = -move
     
-    dir =  dir +1 if dir < 0 else dir
+    dir =  dir +1 if dir < 0 else dir #  음수 일 때 1을 더하는 이유는 위에서 pop으로 뺐기때문에 이미 한번 이동한 것
     
     arr.rotate(dir)
     
+    # rotate(n) 
+    # n > 0 일경우 오른쪽으로 밈(헤더 기준 왼쪽으로 이동 하는 느낌)
+    # n < 0 일경우 왼쪽으로 밈(헤더 기준 오른쪽으로 이동 하는 느낌)
 
 
 print(*ans)
