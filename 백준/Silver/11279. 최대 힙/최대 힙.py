7f48534f602adffca8782af8c189035bfd5cdb33
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-06-29 09:03:28 +0900

    [Silver II] Title: 최대 힙, Time: 204 ms, Memory: 49956 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -1,23 +1,34 @@
-from sys import stdin
-from collections import *
-import heapq
-if __name__ =='__main__':
-    n:int =int(stdin.readline()[:-1])
+from sys import *
+from collections import * 
+from heapq import * 
+from functools import *
+from math import *
+from itertools import *
+
+
+n = int(stdin.readline()[:-1])
+
+
+heap = []
+
+
+for _ in range(n):
     
-    heap=[]
+    m = int(stdin.readline()[:-1])
     
-    for i in range(n):
-        num:int =int(stdin.readline()[:-1])
-        if(num==0):
-            if(len(heap)==0):
-                print(0)
-            else:
-                print(heapq.heappop(heap)[1])
+    if m == 0 :
+        
+        if heap:
+            print(heappop(heap)[1])
+        
         else:
-            heapq.heappush(heap,(-num,num)) #(우선순위,실제 값) -를 붙여 num이 클수록 top에 위치
+            print(0)
+            
+    else:
+        heappush(heap,(-m,m))
+
         
     
-
-   
     
-   
+
+
