17fadd747a6f54c3ae6fad479060b13f180760fa
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-11 18:59:26 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.08 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.swift
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.swift
@@ -10,15 +10,16 @@ func solution(_ number:String, _ k:Int) -> String {
     for n in number {
         let intN = Int(String(n))!
         
-        while !st.isEmpty && st.last! < intN && k > 0 {
-            st.popLast()!
-            k -= 1
+        while !st.isEmpty && st.last! < intN && k > 0 { 
+            //스택이 비어있지 않고, 제거할 수가 남아있으면(k>0) 마지막 값이 현재 값보다 작으면
+            st.popLast()! // 스택에서 제거
+            k -= 1 // 제거 했으므로 하나 감소
         }
         st.append(intN)
         
     }
     
-    while k > 0 {
+    while k > 0 { // 여전히 제거할 수가 남아 있으면 제거
         st.popLast()!
         k -= 1
     }
