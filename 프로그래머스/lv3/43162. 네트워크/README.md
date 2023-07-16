972c2992b68e7f8974c19c87268778d5f5e947a9
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-16 19:56:35 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/README.md
+++ 프로그래머스/lv3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=swift) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 1.05 ms
+메모리: 16.8 MB, 시간: 0.35 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+func find(_ a:Int,_ root: inout [Int]) -> Int {
+    
+    var r = root[a]
+    
+    while( r != root[r] ){
+        r = root[r]
+    }
+    
+    var c = a
+    
+    while( c != root[c] ) {
+        
+        let pc = root[c]
+        root[c] = r
+        c = pc 
+    }
+    
+    return r
+    
+}
+
+func union(_ a:Int,_ b:Int,_ root: inout [Int]) {
+ 
+    let ra = find(a,&root)
+    let rb = find(b,&root)
+    
+    if ra <= rb {
+        root[rb] = ra
+    }
+    else {
+        root[ra] = rb 
+    }
+    
+}
+
+
+func solution(_ n:Int, _ computers:[[Int]]) -> Int {
+    
+    var root:[Int] = [Int](0..<n)
+    
+    for i in (0..<n) {
+        for j in (0..<n){
+            
+            if i == j { continue }
+            
+            if computers[i][j] == 1 {
+                union(i,j,&root)
+            } 
+            
+        }
+        
+        for k in 0..<n {
+            root[k] = find(k,&root)
+        }
+        
+    }
+    
+    return Set(root).count
+}
