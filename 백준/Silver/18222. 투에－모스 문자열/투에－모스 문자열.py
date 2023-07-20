fc50fed9f42ab3cd1e94b7b954b676fe6312e488
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-20 19:23:03 +0900

    [Silver II] Title: 투에-모스 문자열, Time: 84 ms, Memory: 36956 KB -BaekjoonHub

---
+++ 백준/Silver/18222. 투에－모스 문자열/투에－모스 문자열.py
@@ -0,0 +1,13 @@
+from sys import *
+from collections import * 
+from heapq import * 
+from functools import *
+from math import *
+from itertools import *
+
+setrecursionlimit(10**9)
+input = stdin.readline
+
+
+
+print(bin(int(input())-1).count('1')%2)
