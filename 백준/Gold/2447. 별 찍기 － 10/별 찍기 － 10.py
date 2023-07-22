f531cad6eb1d4db399723571921a5648e6690e69
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-22 18:54:42 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 760 ms, Memory: 74848 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,11 +1,29 @@
-def solve(x:int,y:int,n:int,star:list):
+from sys import *
+from collections import * 
+from heapq import * 
+from functools import *
+from math import *
+from itertools import *
+
+setrecursionlimit(10**9)
+input = stdin.readline
+
+
+n = int(input())
+
+board = [ [" " for _ in range(n)] for _ in range(n) ]
+
+
+def printStart(y,x,n):
+    global board
+    
     if(n==3):
         for i in range(3):
             for j in range(3):
                 if(i==1 and j==1):
-                    star[x+i][y+j]=" "
+                    board[y+i][x+j]= " "
                     continue
-                star[x+i][y+j]="*"
+                board[y+i][x+j]= "*"
         return
     
     m=n//3
@@ -13,21 +31,9 @@ def solve(x:int,y:int,n:int,star:list):
         for j in range(3):
             if(i==1 and j==1):
                 continue 
-            solve(x+i*m,y+j*m,m,star)
+            printStart(y+i*m,x+j*m,m)
 
-if __name__ == "__main__":
-    num=int(input())
-    star=[list() for i in range(num)] #2차원 배열 만든 후 
-    for i in range(num):
-        star[i]=[" " for j in range(num)]
-    #1차원 배열 값 ' '으로 초기화 하기
-    solve(0,0,num,star)
+printStart(0,0,n)
 
-    for i in range(num):
-        for j in range(num):
-            print(star[i][j],end="")
-        print()
-
-
-            
-              
+for i in range(n):
+    print("".join(board[i]))
