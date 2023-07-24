f79baaa289632c0423f9127490271bf28ae6af32
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-24 19:51:07 +0900

    Update 민겸 수.py

--- 백준/Silver/21314. 민겸 수/민겸 수.py
+++ 백준/Silver/21314. 민겸 수/민겸 수.py
@@ -11,8 +11,7 @@ input = stdin.readline
 #.      MAX.      MIN
 # MM    11.       10
 # MK    50        15 
-# KM    51
-# KK 
+
 
 s = input()[:-1]
 
@@ -23,13 +22,13 @@ tmp = ""
 
 for c in s:
     
-    if c == "K":
+    if c == "K": 
         
-        if len(tmp) > 0:
-            M += str(int(pow(10,len(tmp)) * 5))
-            m += str(int(pow(10,len(tmp)-1))) + "5"
+        if len(tmp) > 0: # 이전에 쌓인 M이 1개 이상 일때 
+            M += str(int(pow(10,len(tmp)) * 5)) # 최댓값은 5000 꼴
+            m += str(int(pow(10,len(tmp)-1))) + "5" # 최솟값은 15 ,105 꼴 , 10^(m-1) + 5
         
-        else:
+        else: # K만 있을 때는 둘다 += 5
             M += "5"
             m += "5"
         
@@ -39,11 +38,9 @@ for c in s:
         tmp += "M"
 
 
-if tmp:
-    M += "1" * len(tmp)
-    m += "1" + "0" * (len(tmp) -1)
+if tmp: # M이 쌓인채로 반복문이 끝날 경우 
+    M += "1" * len(tmp) #최댓값은 111 패턴 
+    m += "1" + "0" * (len(tmp) -1)  #최솟값은 100000 꼴
 
 print(M)
 print(m)
-    
-    
