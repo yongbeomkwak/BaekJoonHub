8a2cf17d50cf81cb7caea8e2fb055a41468431da
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-26 09:29:09 +0900

    [Silver V] Title: 돌 게임, Time: 88 ms, Memory: 37504 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,18 @@
+from sys import *
+from collections import * 
+from heapq import * 
+from functools import *
+from math import *
+from itertools import *
+
+setrecursionlimit(10**9)
+input = stdin.readline
+
+
+
+n = int(input())
+
+
+
+
+print ("CY" if n % 2 == 0 else "SK")
