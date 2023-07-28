41f23dbae19d65dd67cf19915387112c346586ee
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-28 12:16:26 +0900

    [Silver II] Title: 연속합, Time: 152 ms, Memory: 43452 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -2,35 +2,27 @@ from sys import *
 from collections import * 
 from heapq import * 
 from functools import *
+from math import *
+from itertools import *
+
+#setrecursionlimit(10**9)
+input = stdin.readline
 
 
+n = int(input())
 
+arr = list(map(int,input().split()))
 
-n = int(stdin.readline()[:-1])
-
-arr = list(map(int,stdin.readline()[:-1].split()))
-
-LMIT = 100000
-dp = [ 0  for _ in range(LMIT)]
-
-
+dp = [0 for _ in range(n)]
 
 dp[0] = arr[0]
 
 ans = dp[0]
 
 for i in range(1,n):
-    dp[i] = max(dp[i-1] + arr[i],arr[i])
+    
+    dp[i] =   max(arr[i],dp[i-1] + arr[i]) # 현재 값을 첫 시작으로 할 것 인가 , 이전 누적값과 함께 같이 사용할 것인가 
     ans = max(dp[i],ans)
 
 print(ans)
-
-
-
-
-
-
-
-
-
 
