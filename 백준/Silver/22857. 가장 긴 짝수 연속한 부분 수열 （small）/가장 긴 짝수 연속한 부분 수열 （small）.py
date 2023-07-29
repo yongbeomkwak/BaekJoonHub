fc3d073f46e005dc0f72ad5709a8b475e6221d6d
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-07-29 20:45:04 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 1832 ms, Memory: 242432 KB -BaekjoonHub

---
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.py
@@ -0,0 +1,38 @@
+from sys import *
+from collections import * 
+from heapq import * 
+from functools import *
+from math import *
+from itertools import *
+
+#setrecursionlimit(10**9)
+input = stdin.readline
+
+
+n , k =  map(int,input().split())
+
+arr = [0] + list(map(int, input().split()))
+dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+ans = 0
+for i in range(1,n+1):
+    
+    arr[i] %= 2
+    for j in range(k+1):
+        
+        if arr[i] == 0: # 짝수
+        
+            dp[i][j] = dp[i-1][j] + 1 # 이전 값 + 1
+        
+        if j != 0 and arr[i]: #홀수일 때 
+
+            dp[i][j] = dp[i-1][j-1] # 이전(i-1) j-1값 그대로 이어 받음 
+        
+    
+    
+    tmp = max(dp[i])    
+    ans = max(ans,tmp)
+    
+
+
+print(ans)
