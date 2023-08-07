c4734ff2196b300ecdab632ee18223bcc3f74ef1
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-08-07 13:47:37 +0900

    [Silver I] Title: 주지수, Time: 892 ms, Memory: 85116 KB -BaekjoonHub

--- 백준/Silver/15724. 주지수/주지수.py
+++ 백준/Silver/15724. 주지수/주지수.py
@@ -9,35 +9,20 @@ from itertools import *
 input = stdin.readline
 
 n,m = map(int,input().split())
+area = [[0 for _ in range(m+1)]]
 
-board = []
+for _ in range(n):
+    area.append([0]+list(map(int,input().split())))
 
-dp = [[0 for _ in range(m)] for _ in range(n)]
+dp = [[0]*(m+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + area[i][j] # 바로 위 + 바로 왼쪽 - 왼쪽위 대각선 + 현재 
 
-for i in range(n):
-    board.append(list(map(int,input().split())))
+for _ in range(int(input())):
+    x,y,x2,y2 = map(int,input().split())
     
-    for j in range(m):
-        if j == 0:
-            dp[i][j] = board[i][j]
-        
-        else:
-            dp[i][j] += board[i][j] + dp[i][j-1]
+    x -=1
+    y -=1
 
-    
-k = int(input())
-
-
-for _ in range(k):
-    x1,y1,x2,y2 = map(int,input().split())
-    
-    y1 -=1
-    y2 -=1
-    x1 -=1
-    
-    ans = 0
-    for h in range(x1,x2):
-        ans += dp[h][y2] - dp[h][y1] + board[h][y1] # y2까지 누적합 - y1까지 누적합 + 손실된 y1복구 
-                                                    # (1,3) (1,5) , 5 4 3 2 1 - 3 2 1 , 3손실됨 + 3 
-        
-    print(ans)
+    print(dp[x2][y2] - dp[x][y2] - dp[x2][y] + dp[x][y]) 
