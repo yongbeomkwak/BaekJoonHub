d2b1952b5e748f81f474c061ae1dc7b97a4c8419
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-08-10 18:53:38 +0900

    [Gold V] Title: 진우의 달 여행 (Large), Time: 2576 ms, Memory: 242656 KB -BaekjoonHub

--- 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.py
+++ 백준/Gold/17485. 진우의 달 여행 （Large）/진우의 달 여행 （Large）.py
@@ -27,15 +27,15 @@ for i in range(n):
 
 for i in range(1,n):
     for j in range(m):
-        if j == 0:
-            dp[i][j][2] = min(dp[i-1][j+1][0],dp[i-1][j][1]) + arr[i][j]
-            dp[i][j][1] = dp[i-1][j+1][0] + arr[i][j] 
+        if j == 0: # j ==0 이면 왼쪽아래는 못감 
+            dp[i][j][2] = min(dp[i-1][j+1][0],dp[i-1][j][1]) + arr[i][j]  # 다음이 오른쪽아래 = 이전 왼쪽 아래 또는 이전 바로아래  
+            dp[i][j][1] = dp[i-1][j+1][0] + arr[i][j] # 다음이 바로 아래 = 이전 왼쪽 위에서 
         
-        elif j == m-1:
-            dp[i][j][1] = dp[i-1][j-1][2] + arr[i][j]
-            dp[i][j][0] = min(dp[i-1][j][1],dp[i-1][j-1][2]) + arr[i][j]
+        elif j == m-1: # j == m-1면 다음은 오른쪽 아래로 못감
+            dp[i][j][1] = dp[i-1][j-1][2] + arr[i][j] # 다음이 바로 아래 = 이전 왼쪽 위에서 
+            dp[i][j][0] = min(dp[i-1][j][1],dp[i-1][j-1][2]) + arr[i][j] # 다음이 왼쪽 아래 = 이전 바로 위 또는 왼쪽 위 
         
-        else:
+        else: # 위 2 경우가 아니면 다음 방향마다 2가지씩 고려 
             dp[i][j][0] = min(dp[i-1][j][1],dp[i-1][j-1][2]) + arr[i][j]
             dp[i][j][1] = min(dp[i-1][j+1][0],dp[i-1][j-1][2]) + arr[i][j]
             dp[i][j][2] = min(dp[i-1][j+1][0],dp[i-1][j][1]) + arr[i][j]
