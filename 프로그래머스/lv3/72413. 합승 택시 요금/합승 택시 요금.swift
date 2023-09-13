f1e5f914fab5988d03322a6102a57c5343b8d9f9
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-09-13 19:48:43 +0900

    [level 3] Title: 합승 택시 요금, Time: 56.87 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/72413. 합승 택시 요금/합승 택시 요금.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+func solution(_ n:Int, _ s:Int, _ a:Int, _ b:Int, _ fares:[[Int]]) -> Int {
+    
+    let maxFare = 200 * 100000
+    var graph = [[Int]](repeating: [Int](repeating: maxFare, count: n + 1), count: n + 1)
+    var answer = Int.max
+    
+    for f in fares {
+        
+        let from = f[0]
+        let to = f[1]
+        let cost = f[2]
+        
+        graph[from][to] = cost
+        graph[to][from] = cost 
+        
+    }
+    
+    for i in 1...n {
+        graph[i][i] = 0
+    }
+    
+    for k in 1...n {
+        for i in 1...n {
+            for j in 1...n {
+                if graph[i][j] > graph[i][k] + graph[k][j] {
+                    graph[i][j] = graph[i][k] + graph[k][j]
+                }
+            }
+        }
+    }
+    
+    // 중간지점을 하나씩 설정하여 최솟값을 구함. 출발지에서 각자 출발하는 경우도 포함 (mid == s)
+    for mid in 1...n {
+        answer = min(answer, graph[s][mid] + graph[mid][a] + graph[mid][b])
+    }
+
+    return answer
+    
+    return 0
+}
