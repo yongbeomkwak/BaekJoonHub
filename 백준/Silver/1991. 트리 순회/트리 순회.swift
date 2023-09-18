abd128176b5a247522b6e68b02cf811bd5ec4292
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-09-18 20:00:54 +0900

    Update 트리 순회.swift

--- 백준/Silver/1991. 트리 순회/트리 순회.swift
+++ 백준/Silver/1991. 트리 순회/트리 순회.swift
@@ -35,6 +35,47 @@ func inOrder(_ node:String) {
     inOrder(adj[node]?[1] ?? ".") // 오른쪽
 }
 
+func inOrder2(_ node:String) -> String { // 스택을 이용한 중위 순회
+    
+//    if node == "." {
+//        return
+//    }
+//    inOrder(adj[node]?[0] ?? ".") // 왼쪽
+//    result.append(node)
+//    inOrder(adj[node]?[1] ?? ".") // 오른쪽
+    
+    var stack:[String] = []
+    var result:String = ""
+    
+    var node:String = node
+    
+    while !stack.isEmpty || node != "." {
+        
+        if node != "." {
+            
+            stack.append(node) // 담고
+            node = adj[node]?[0] ?? "." // 왼쪽으로 이동
+            
+            continue
+        }
+        
+        else {
+            
+            let element = stack.popLast() ?? "."
+            
+            result += element
+        
+            node = adj[element]?[1] ?? "." //오른쪽 노드
+        }
+        
+        
+    }
+    
+    
+    return result
+}
+
+
 func postOrder(_ node:String) {
     
     if node == "." {
