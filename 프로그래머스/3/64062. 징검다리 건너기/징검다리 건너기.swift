398bbfca12ba7a0bd9bedc149119564fbc729798
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-11-21 21:03:59 +0900

    [level 3] Title: 징검다리 건너기, Time: 46.22 ms, Memory: 25.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+func countZero(_ stones:[Int],_ limit:Int, _ k:Int) -> Bool {
+ 
+    var stone = 0
+    for i in 0..<stones.count {
+        if stones[i] - limit <= 0 { stone += 1 }
+        else { stone = 0 }
+
+        if stone >= k { return false }
+    }
+
+    return true
+}
+
+func solution(_ stones:[Int], _ k:Int) -> Int {
+    
+    
+    var l: Int =  stones.min()!
+    var r: Int =  stones.max()!
+    
+    var mid: Int {
+        return (l+r)/2
+    }
+    
+    var ans:Int = 0
+    
+    while l<=r {
+        
+        if countZero(stones,mid,k) { // 건넘
+            l = mid+1
+            
+        } else {
+            r = mid-1
+        }
+        
+    }
+    
+    
+    return l
+    
+
+}
