0792ef2a70e62a32a76342cd158ea89a83acf8f3
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-11-24 16:07:14 +0900

    Update 주차 요금 계산.swift

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.swift
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.swift
@@ -16,49 +16,50 @@ func solution(_ fees:[Int], _ records:[String]) -> [Int] {
     
     var ans: [Int] = []
     
-    var parking:[String:Int] = [:]
+    var parking:[String:Int] = [:] // [차번호: 입차시간]
     
-    var cost:[String:Int] = [:]
+    var cost:[String:Int] = [:] // [차번호: 최종 누적 시간]
     
     for record in records {
         let input = record.split{$0 == " "}.map{String($0)}
         let carNum = input[1]
         let min = timeToMin(input[0])
         
-        if input[2] == "IN" {
+        if input[2] == "IN" { // 입차
             
             parking[carNum] = min
             
         } else {
             
-            let gap = min - parking[carNum]!
-            parking[carNum] = nil
+            let gap = min - parking[carNum]! // 주차시간 
+            parking[carNum] = nil //출차
+        
             
-            // let finalCost = 
-            
-            cost[carNum] = cost[carNum,default:0] + gap
+            cost[carNum] = cost[carNum,default:0] + gap // 누적
         }
 
     }
     
     parking.forEach { // 아직 출차 하지 않은 차들 
         
-        let gap = timeToMin("23:59") - $0.value // 마감시간 - 입차시간
+        let gap = timeToMin("23:59") - $0.value // 마감시간 - 입차시간 = 주차시간
         
-        cost[$0.key] = cost[$0.key,default:0] + gap
+        cost[$0.key] = cost[$0.key,default:0] + gap  // 누적
     }
     
   
     
     func calcFinalCost(_ time:Int) -> Int {
-        return (time > baseMin ? baseCost + Int(ceil(Double(time-baseMin)/Double(unitMin))) * unitCost : baseCost) 
+        return (time > baseMin ? baseCost + Int(ceil(Double(time-baseMin)/Double(unitMin))) * unitCost : baseCost)  
+        
+        // 기본 시간보다 크면 , 기본 요금 + (올림 처리 * 단위요금) , 그렇지 않으면 기본 요금만
     }
     
     cost.forEach{
-        cost[$0.key] = calcFinalCost($0.value)
+        cost[$0.key] = calcFinalCost($0.value) // 누적시간을 최종 금액으로 변환 
     }
     
-    var sortedCar = cost.keys.sorted().map{cost[$0]!}
+    var sortedCar = cost.keys.sorted().map{cost[$0]!} // 차 번호로 정렬 후 해당 금액 추출
 
     return sortedCar
 }
