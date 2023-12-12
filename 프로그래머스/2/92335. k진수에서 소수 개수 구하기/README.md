3bc6276f362adcd08e20043b33a3a3db95442a87
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-12-12 13:36:13 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.15 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] k진수에서 소수 개수 구하기 - 92335 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92335) 
 
 ### 성능 요약
 
-메모리: 16.2 MB, 시간: 0.13 ms
+메모리: 16.3 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 21:11:9
+2023년 12월 2일 13:36:12
 
 ### 문제 설명
 
--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.swift
@@ -1,31 +1,41 @@
 import Foundation
 
-func solution(_ n:Int, _ k:Int) -> Int {
+func isPrime(_ n: Int) -> Bool {
     
-    var s = String(n,radix:k)
-    var arr = s.split{ $0 == "0"}.map{Int(String($0))!}
+    if n == 1 {
+        return false 
+    }
     
-    var isPrime: ((Int) -> Bool) = { n -> Bool in 
-        
-        var end = Int(sqrt(Double(n)))
-        
-        if n == 1 {
+    if n < 4 {
+        return true 
+    }
+    
+    let end = Int(sqrt(Double(n)))
+    
+    for i in 2...end {
+        if (n%i) == 0 {
             return false 
-        } else if n == 2 || n == 3 {
-            return true 
         }
-        
-        for i in 2..<end+1 {
-            
-            if n%i == 0 {
-                return false
-            }
-            
-        }
-        
-        return true
     }
     
     
-    return arr.filter{isPrime($0)}.count
+    
+    return true 
+    
+}
+
+func solution(_ n:Int, _ k:Int) -> Int {
+    
+    var list = String(n,radix:k).split{$0 == "0"}.map{Int($0)!}
+    
+    var ans:Int = 0
+    
+    print(list)
+    for i in list {
+        if isPrime(i) {
+            ans += 1
+        }    
+    }
+    
+    return ans
 }
