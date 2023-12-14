1b240105772244cd6a07a36b0663feb2fb9dfcc7
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-12-14 16:18:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 112.03 ms, Memory: 25.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -1,52 +1,79 @@
 import Foundation
 
-
-
-func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+struct Queue<T> {
     
-    var totalQueue = queue1 + queue2 
+    var enque:[T]
+    var deque:[T] = []
     
-    var sum1 = queue1.reduce(0,+)
-    
-    var sum2 = queue2.reduce(0,+)
-    
-    var target = (sum1+sum2)/2
-    
-    if target < queue1.max()! || target < queue2.max()! || (sum1+sum2)%2 != 0 {
-        // 타겟을 넘어가는 숫자가 존재하거나 , 타겟이 홀 수 이면 아예 불가능
-        return -1
+    public init(_ enque:[T]) {
+        self.enque = enque
     }
     
-    var left = 0
-    var right = queue1.count // 투포인터 
-    var answer = 0
-    
-    while (left <= right) && (right < totalQueue.count) { // 범위를 벗어나거나면
+    var first: T? {
         
-        if sum1 == target {
-            return answer
+        if deque.isEmpty {
+            return enque.first
         }
         
-        if sum1 > sum2 {  // 왼쪽이 큼
+        return deque.last
+        
+    }
+    
+    mutating func push(_ element:T) {
+        enque.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        
+        if deque.isEmpty {
+            deque = enque.reversed()
+            enque.removeAll()
+        }
+        
+        return deque.removeLast()
+        
+    }
+    
+}
+
+
+
+func solution(_ queue1:[Int], _ queue2:[Int]) -> Int{
+    
+    let limit = queue1.count * 3 
+    var ans:Int = 0
+    
+    var q1 = queue1.reduce(0,+)
+    var q2 = queue2.reduce(0,+)
+    
+    if (q1+q2) % 2 != 0 {
+        return -1 
+    }
+
+    var queue1 = Queue(queue1)
+    var queue2 = Queue(queue2)
+    
+    while (ans < limit) {
+        
+        if q1 ==  q2 {
             
-            sum1 -= totalQueue[left]
-            sum2 += totalQueue[left]
-            left += 1
+            return ans
             
+        } else if q1 > q2  {
+            let pop = queue1.pop()!
+            q1 -= pop
+            q2 += pop
+            queue2.push(pop)
+        } else {
+            let pop = queue2.pop()!
+            q2 -= pop
+            q1 += pop
+            queue1.push(pop)
         }
         
-        else { // 오른쪽이 큼 
-            sum2 -= totalQueue[right]
-            sum1 += totalQueue[right]
-            right += 1
-        }
-        
-        answer += 1
-        
-    } 
+        ans += 1   
+    }
     
     
-    
-    
-    return  -1
+    return -1
 }
