e7051e1997ceea36fa728d52dc64df25305085df
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-12-20 15:01:22 +0900

    [level 3] Title: 합승 택시 요금, Time: 79.73 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/README.md
+++ 프로그래머스/3/72413. 합승 택시 요금/README.md
@@ -1,10 +1,10 @@
 # [level 3] 합승 택시 요금 - 72413 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72413) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 68.99 ms
+메모리: 16.4 MB, 시간: 79.73 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 6일 12:8:7
+2023년 12월 3일 15:1:19
 
 ### 문제 설명
 
--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.swift
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.swift
@@ -2,36 +2,37 @@ import Foundation
 
 func solution(_ n:Int, _ s:Int, _ a:Int, _ b:Int, _ fares:[[Int]]) -> Int {
     
-    let max = 2000000
-    
-    var adj:[[Int]] = [[Int]](repeating:[Int](repeating:max,count:n+1),count: n+1)
-    
-    for i in 0...n {
-        adj[i][i] = 0
-    }
+    var graph:[[Int]] = [[Int]](repeating:[Int](repeating:20000000,count:n+1),count:n+1)
     
     for fare in fares {
         
         let c = fare[0]
         let d = fare[1]
         let f = fare[2]
-        adj[c][d] = f
-        adj[d][c] = f 
+        
+        graph[c][d] = f
+        graph[d][c] = f
     }
     
+    
+    // 플로이드 와샬
     for k in 1...n {
         for i in 1...n {
             for j in 1...n {
-                adj[i][j] = min(adj[i][j],adj[i][k]+adj[k][j])
+                
+                if graph[i][j] > graph[i][k] + graph[k][j] {
+                    graph[i][j] = graph[i][k] + graph[k][j]
+                }
+                
             }
         }
     }
     
-  
-    var cost = Int.max
+    var viaWay = min(graph[s][b] + graph[b][a], graph[s][a] + graph[a][b] ) // 첫 출발은 , 바로 a 또는 b 집에 간다음 남은 사람 집감 (다른 지점 경유 x) 
+    
     for i in 1...n {
-        cost = min(cost,adj[s][i]+adj[i][a]+adj[i][b]) // i까지 가고 따로 가는거
+        viaWay = min(viaWay,graph[s][i] + graph[i][a] + graph[i][b]) // 이후 모든 경유지 이후 그 경유지에서 각자 감
     }
     
-    return min(cost,adj[s][a]+adj[s][b]) // 출발 지점에서 아예 따로 출발
+    return min(graph[s][a] + graph[s][b],viaWay) // 마지막으로 출발지부터 따로 가는 경우랑 비교 
 }
