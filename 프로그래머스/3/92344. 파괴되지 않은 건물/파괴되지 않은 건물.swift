f43e1eb897439851d7dd513b559463155eda6bde
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-12-22 21:01:40 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 354.60 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.swift
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.swift
@@ -1,62 +1,40 @@
 import Foundation
 
-func effect (_ degreeResult: inout [[Int]], _ skill:[Int]) {
+func effect(_ skill:[Int],_ acc: inout [[Int]] ) {
     
     let r1 = skill[1]
+    let r2 = skill[3]+1
     let c1 = skill[2]
-    let r2 = skill[3]
-    let c2 = skill[4]
-    let degree = skill[0] == 1 ? -skill[5] : skill[5]
-    
-    /*
-     이팩트가 가해지는 범위
-     r1 = 1 , r2 = 2
-     c1 = 1 , c2 = 2 일 때
-     d = degree
-    
-        0  1  2  3
-    
-    0           
-    
-    1      d     -d
-    
-    2      
-    
-    3      -d     d
+    let c2 = skill[4]+1
     
     
-       
-    */
+    let d = skill[0] == 1 ? -skill[5] : skill[5] 
     
-    degreeResult[r1][c1] += degree
-    degreeResult[r2+1][c2+1] += degree  
-    degreeResult[r2+1][c1] -= degree
-    degreeResult[r1][c2+1] -= degree 
+    
+    acc[r1][c1] += d 
+    acc[r1][c2] -= d
+    acc[r2][c1] -= d
+    acc[r2][c2] += d 
+    
     
 }
 
 func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
 
-    /// type: 1(적), 2(아군)
-    /// r1 -> r2 , c1 -> c2
-    /// degree -> amount
-    
-    var board = board
+    var board = board 
     let n = board.count
     let m = board[0].count
+    var acc = [[Int]](repeating:[Int](repeating:0,count:m+1),count:n+1)
     
-    var degreeResult: [[Int]] = [[Int]](repeating:[Int](repeating:0,count:m+1), count: n+1)
     
-    
-    for _skill in skill {
-        
-        effect(&degreeResult, _skill)
+    for sk in skill {
+        effect(sk,&acc)
     }
-    
+
     
     for r in 0..<n {
         for c in 1...m {
-            degreeResult[r][c] += degreeResult[r][c-1]
+            acc[r][c] += acc[r][c-1]
         } 
     }
     //가로 누적합
@@ -64,7 +42,7 @@ func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
     for c in 0..<m {
         for r in 1...n {
             
-            degreeResult[r][c] += degreeResult[r-1][c]
+            acc[r][c] += acc[r-1][c]
         }
     }
     
@@ -75,7 +53,7 @@ func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
     for r in 0..<n {
         for c in 0..<m {
         
-            if board[r][c] + degreeResult[r][c] > 0 { 
+            if board[r][c] + acc[r][c] > 0 { 
                 result += 1
             }
             
@@ -84,4 +62,6 @@ func solution(_ board:[[Int]], _ skill:[[Int]]) -> Int {
     
     
     return result
+    
+
 }
