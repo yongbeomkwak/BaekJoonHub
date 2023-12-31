d2c3c4470438b9b527b3c9dfa35903a900ebb223
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2023-12-31 15:41:06 +0900

    [level 3] Title: 양과 늑대, Time: 0.50 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.swift
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.swift
@@ -2,46 +2,41 @@ import Foundation
 
 func solution(_ info:[Int], _ edges:[[Int]]) -> Int {
     
-    var visited: [Bool] = [Bool](repeating:false, count: info.count)
+    // 0 = 양 , 1 = 늑대
+    
+    var visited: [Bool] = [Bool](repeating:false,count:info.count)
+    var maxCnt: Int = 1 
     
     
-    visited[0] = true
-    var answer = 0
+    func dfs(_ sheep:Int, _ wolf: Int) {
     
-    func dfs(_ sheep: Int, _ wolf: Int) {
-        
-        if sheep <= wolf {
-            return 
+        if sheep > wolf {
+            maxCnt = max(maxCnt,sheep)
         } else {
-            answer = answer < sheep ? sheep : answer 
+            return 
         }
         
         
+         // 모든 간선을 순회하면서
         for edge in edges {
-            
-            let parent = edge[0]
-            let child = edge[1]
-            
-            if visited[parent] , !visited[child] { // 부모는 방문했고,  자식 아직 방문 안했으면 탐색 가능성 o
-                
+            let parent = edge[0], child = edge[1]
+            // 부모는 방문했고 자식은 방문하지 않은 edge를 찾는다.
+                // 부모는 이미 방문했으므로 현재 node에서 갈 수 있다. (돌아서라도)
+            if visited[parent] && !visited[child] {
                 visited[child] = true
                 if info[child] == 0 {
-                    dfs(sheep+1,wolf)
+                    dfs(sheep + 1,wolf)
+                } else {
+                    dfs(sheep, wolf + 1)
                 }
-                
-                else {
-                    dfs(sheep,wolf+1)
-                }
-                
                 visited[child] = false
-                
             }
-            
         }
         
     }
     
+    visited[0] = true 
     dfs(1,0)
     
-    return answer
+    return maxCnt
 }
