6701cd1a3ffa680859d70e092c037fdf3a7afb4a
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-01-01 22:13:39 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 57.84 ms, Memory: 24.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.swift
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.swift
@@ -1,66 +1,57 @@
 import Foundation
 
 extension Array where Element == Int {
-    
-    mutating func removeZeroToSuffix() {
+
+    mutating func removeZero() {
         
         while self.last ?? -1 == 0 {
             self.removeLast()
-        }
+        }  
         
     }
-    
 }
-
 
 func solution(_ cap:Int, _ n:Int, _ deliveries:[Int], _ pickups:[Int]) -> Int64 {
     
     var del = deliveries
-    var pick = pickups 
-    var answer:Int = 0
+    var pic = pickups 
     
-    del.removeZeroToSuffix()
-    pick.removeZeroToSuffix()
     
-
+    del.removeZero()
+    pic.removeZero()
     
-    while !del.isEmpty || !pick.isEmpty {
-        
-        answer += max(del.count, pick.count)*2 
-        // 배달과 수거 거리중 큰 것을 왕복(*2)
+    var ans: Int = 0 
+    
+    while !del.isEmpty || !pic.isEmpty {
         
         var nowCap = cap
+        ans += max(del.count,pic.count)*2 // 현재 순회에서 갈 가장 먼 집 왕복
         
-        while !del.isEmpty  { // 배달 ㄱㄱ 
+        while !del.isEmpty {
             
-            if del.last ?? 0  > nowCap { // 들 수 있는 상자보다 많을 때 
-                del[del.count-1] -= nowCap 
-                break // 다시
+            if del.last ?? 0 > nowCap {
+                del[del.count-1] -= nowCap  
+                break 
             }
-            
-            else { // 적거나 같으면 이 집은 다시 안와도 됨, 그만 큼 전해주고 , 목적지에서 뺌 
+            else {
                 nowCap -= del.removeLast()
             }
-        
         }
         
-        nowCap = cap
-        
-        while !pick.isEmpty  { // 수거 ㄱㄱ 
+        nowCap = cap 
+        while !pic.isEmpty {
             
-            if pick.last ?? 0 > nowCap { //들 수 있는 상자보다 많을 때 
-                pick[pick.count-1] -= nowCap
-                break // 다시 복귀
+            if pic.last ?? 0 > nowCap {
+                pic[pic.count-1] -= nowCap 
+                break 
             }
-            
             else {
-                nowCap -= pick.removeLast()
+                nowCap -= pic.removeLast()
             }
-            
         }
+        
         
     }
-
     
-    return Int64(answer)
+    return Int64(ans)
 }
