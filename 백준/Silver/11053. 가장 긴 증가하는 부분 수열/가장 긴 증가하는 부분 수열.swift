ff88a5f73ccd8aba108e71550302badf010afca4
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-01-02 17:18:58 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 16 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -0,0 +1,17 @@
+let n = Int(readLine()!)!
+
+let arr = readLine()!.split{$0 == " "}.map({Int($0)!})
+
+var dp: [Int] = [Int](repeating: 1, count: n+1)
+
+
+for i in 0..<n {
+    for j in 0...i {
+        
+        if arr[i] > arr[j] {
+            dp[i] = max(dp[i],dp[j]+1)
+        }
+    }
+}
+
+print(dp.max()!)
