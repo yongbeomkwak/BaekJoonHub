d64d8861d76b7bb472f0822d2e695538ab74c4c3
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-03-18 19:37:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 41.53 ms, Memory: 29.9 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.swift
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.swift
@@ -1,78 +1,52 @@
 import Foundation
 
-struct Queue<T> {
-    
-    var enque: [T]
-    var deque: [T] = []
-    
-    var isEmpty: Bool {
-        return enque.isEmpty && deque.isEmpty 
-    }
-    
-    
-    init(_ enque:[T]) {
-        self.enque = enque
-    }
-    
-    mutating func push(_ element: T) {
-        enque.append(element)
-    }
-    
-    mutating func pop() -> T? {
-        
-        if deque.isEmpty {
-            deque = enque.reversed()
-            enque.removeAll()
-        }
-        
-        return deque.removeLast()
-    }
-    
-}
-
-
 func solution(_ n:Int, _ edge:[[Int]]) -> Int {
     
-    var graph: [[Int]] = Array(repeating: [], count:n+1)
+    var graph: [[Int]] = [[Int]](repeating:[],count:n+1)
     
-    var dist: [Int] = [Int](repeating:Int.max, count: n+1)
-    
-    for edg in edge {
+    for e in edge {
         
-        let e: Int = edg[0]
-        let v: Int = edg[1]
+        let a = e[0]
+        let b = e[1]
         
-        graph[e].append(v)
-        graph[v].append(e)
+        graph[a].append(b)
+        graph[b].append(a)
         
     }
     
-    var q = Queue<Int>([])
+    var dist: [Int] = [Int](repeating:1000000,count:n+1)
     
-    q.push(1)
-    dist[1] = 0
-    dist[0] = 0
+    var queue: [Int] = []
     
-
+    queue.append(1)
+    dist[0] = -1
+    dist[1] = 0 
     
-    while !q.isEmpty {
+    var index:Int = 0 
+    
+    var result = 0
+    
+    while index < queue.count {
+        let now = queue[index]
         
-        let front = q.pop()!
- 
-        
-        for next in graph[front] {
+        for next in graph[now] {
             
-            if dist[front] + 1 < dist[next] {
+            if dist[next] > dist[now] + 1 {
+                dist[next] = dist[now]+1
+                queue.append(next)
                 
-                dist[next] = dist[front] + 1
-                q.push(next)
+                result = max(result,dist[next])
+                
             }
             
         }
+        
+        index += 1 
     }
     
-    let `max` = dist.max()!
+    print(dist)
     
     
-    return dist.filter{$0 == max}.count
+    
+    return dist.filter{$0 == result}.count
 }
