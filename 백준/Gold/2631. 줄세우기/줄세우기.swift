dd7c6926bf169df96c2f3820f1e554cc7eccb5e6
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-03-21 12:36:29 +0900

    [Gold IV] Title: 줄세우기, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.swift
+++ 백준/Gold/2631. 줄세우기/줄세우기.swift
@@ -1,22 +1,23 @@
-let n = Int(readLine()!)!
+let n  = Int(readLine()!)!
 
-var numbers : [Int] = [Int](repeating: 0, count: n+1)
-var dp : [Int] = [Int](repeating: 1, count: n+1) // dp[i] = i까지 최장 증가 부분 수열
- 
-for i in 1...n {
-    numbers[i] = Int(readLine()!)!
+var arr: [Int] = []
+
+for _ in 0..<n {
+    arr.append(Int(readLine()!)!)
 }
 
+var dp = [Int](repeating: 1, count: n)
 
-for i in 2...n {
-    
-    for j in 1..<i {
-        
-        if numbers[i] > numbers[j] && dp[i] < dp[j] + 1 {
-            dp[i] = dp[j]+1
+var result: Int =  1
+
+for i in 1..<n {
+    for j in 0..<i {
+        if arr[i] > arr[j] {
+            dp[i] = max(dp[i],dp[j]+1)
+            result = max(result,dp[i])
         }
         
     }
 }
 
-print(n-dp.max()!)
+print(n-result)
