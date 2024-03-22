8924bb78e3fa4991c0bf83697c7aef1368c74f16
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-03-22 20:29:59 +0900

    [Silver I] Title: 회전 초밥, Time: 28 ms, Memory: 69508 KB -BaekjoonHub

--- 백준/Silver/2531. 회전 초밥/회전 초밥.swift
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.swift
@@ -2,56 +2,44 @@ let input = readLine()!.split{$0 == " "}.map{Int($0)!}
 
 let (n,d,k,c) = (input[0],input[1],input[2],input[3])
 
-var sushies: [Int] = []
-
-var eatenDish: [Int:Int] = [:]
+var arr: [Int] = []
 
 for _ in 0..<n {
-    sushies.append(Int(readLine()!)!)
+    arr.append(Int(readLine()!)!)
 }
 
-var ans : Int = 0
+var hash: [Int:Int] = [:]
+
+hash[c] = 1
 
 var left = 0
 var right = 0
 
-eatenDish[c] = 1 // 쿠펀꺼는 일단 1개 추가
-
-
 while right < k {
-    eatenDish[sushies[right],default: 0] += 1
+    
+    hash[arr[right],default: 0] += 1
+    
     right += 1
 }
 
-var result: Int = 0
+var result = hash.count
 
-while left < n { // left가 끝에 도달할 때 까지
-    
-    result = max(result,eatenDish.count)
-    
-    let l = sushies[left]
-    let r = sushies[right]
-    
-    eatenDish[l]! -= 1
-    if eatenDish[l] == 0 {
-        eatenDish[l] = nil
+while left < n {
+
+    if hash[arr[left],default: 0] == 1 {
+        hash[arr[left]] = nil
+    } else {
+        hash[arr[left],default: 0] -= 1
     }
     
     
-    eatenDish[r,default: 0] += 1
-    
     left += 1
+    
+    hash[arr[right],default:0] += 1
     right += 1
-    right %= n // 인덱스 벗어남 방지
+    right %= n
+    result = max(result,hash.count)
+    
 }
 
 print(result)
-    
-
-    
- 
-
-
-
-
-
