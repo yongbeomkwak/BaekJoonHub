cbd719f331fdbe427684c74ba5c5a599d2d366eb
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-03-22 14:47:40 +0900

    [Gold V] Title: 배열 돌리기 2, Time: 84 ms, Memory: 69848 KB -BaekjoonHub

--- 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.swift
+++ 백준/Gold/16927. 배열 돌리기 2/배열 돌리기 2.swift
@@ -1,86 +1,85 @@
-let nmr = readLine()!.split{$0 == " "}.map{Int($0)!}
+var nmr = readLine()!.split{$0 == " "}.map{Int($0)!}
 
 let (n,m,r) = (nmr[0],nmr[1],nmr[2])
 
-var board : [[Int]] = []
+var board: [[Int]] = []
 
 for _ in 0..<n {
     board.append(readLine()!.split{$0 == " "}.map{Int($0)!})
 }
 
+func rotate(_ x:Int,_ y: Int,_ h:Int,_ w:Int,_ rotateCnt: Int,_ isCounter: Bool) {
+    
+    
+    
+    let loopCounter = min(x+h,y+w)/2
+        
 
-func rotateCounterClock(_ roateCnt: Int) {
     
-    
-    let loopCount = min(n,m)/2
-    
-    
-    
-    for loop in 0..<loopCount { // 루프는 현재 가르키고 있는 태두리와 같다
-        
+    for l in 0..<loopCounter {
         var tmp = [Int]()
+        let left = y+l
+        let top = x+l
+        let rihgt = y+w-1-l
+        let bottom = x+h-1-l
         
-        let endOfRight = m-loop-1
-        let endOfBottom = n-loop-1
-        
-        for i in loop..<endOfRight { // 루프의 가장위 가로 담기
-            tmp.append(board[loop][i])
-        }
-      
-        for i in loop..<endOfBottom {
-            tmp.append(board[i][endOfRight]) // 오른쪽 끝 세로 담기
-        }
-    
-        // 아래쪽 끝을 오른쪽부터 담기
-        for i in stride(from: endOfRight-1, through:loop, by: -1) {
-            tmp.append(board[endOfBottom][i+1])
+        for i in stride(from: left, to: rihgt, by: 1) {
+            tmp.append(board[top][i])
         }
         
-        //왼쪽 끝을 아래부터 담기
-        for i in stride(from: endOfBottom-1, through: loop, by: -1) {
-            tmp.append(board[i+1][loop])
+        for i in stride(from: top, to: bottom, by: 1) {
+            tmp.append(board[i][rihgt])
         }
         
+        for i in stride(from: rihgt, to: left, by: -1) {
+            tmp.append(board[bottom][i])
+        }
+        
+        for i in stride(from: bottom, to: top, by: -1) {
+            tmp.append(board[i][left])
+        }
         
         let len = tmp.count
-        let move = roateCnt%len
         
-        tmp = Array(tmp[move..<len] + tmp[0..<move])
+        if len == 0 {
+            break
+        }
+        
+        let move = rotateCnt%len
+        
+        tmp = isCounter ?  Array(tmp[move..<len] + tmp[0..<move]) : Array(tmp[len-move..<len] + tmp[0..<len-move])
         
         var k = 0
         
-        
-        for i in loop..<endOfRight { // 루프의 가장위 가로 담기
-            board[loop][i] = tmp[k]
-            k+=1
-        }
-      
-        for i in loop..<endOfBottom {
-            board[i][endOfRight] = tmp[k] // 오른쪽 끝 세로 담기
-            k+=1
+        for i in stride(from: left, to: rihgt, by: 1) {
+            board[top][i] =  tmp[k]
+            k += 1
         }
         
-        
-        // 아래쪽 끝을 오른쪽부터 담기
-        for i in stride(from: endOfRight-1, through:loop, by: -1) {
-            board[endOfBottom][i+1] = tmp[k]
-            k+=1
+        for i in stride(from: top, to: bottom, by: 1) {
+            board[i][rihgt] =  tmp[k]
+            k += 1
         }
         
-        //왼쪽 끝을 아래부터 담기
-        for i in stride(from: endOfBottom-1, through: loop, by: -1) {
-            board[i+1][loop] = tmp[k]
-            k+=1
+        for i in stride(from: rihgt, to: left, by: -1) {
+            board[bottom][i] = tmp[k]
+            k += 1
         }
+        
+        for i in stride(from: bottom, to: top, by: -1) {
+            board[i][left] = tmp[k]
+            k += 1
+        }
+        
+        
         
         
     }
     
-    
 }
 
 
-rotateCounterClock(r)
+rotate(0, 0, n, m, r, true)
 
 for i in 0..<n {
     print(board[i].map{String($0)}.joined(separator: " "))
