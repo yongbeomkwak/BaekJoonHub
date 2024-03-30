36c2d8b2e4154fb5f92e88fde01731e7193ccb37
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-03-30 10:02:03 +0900

    [Gold V] Title: 링크와 스타트, Time: 388 ms, Memory: 69108 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.swift
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.swift
@@ -1,70 +1,38 @@
-let n = Int(readLine()!)!
+let n = Int(String(readLine()!))!
+var arr: [[Int]] = [[]]
+var visited = Array(repeating: false, count: n + 1)
+var result = Int.max
 
-var s: [[Int]] = []
-
-for _ in 0..<n {
-    s.append(readLine()!.split{$0 == " "}.map{Int($0)!})
+for _ in 1...n {
+    arr.append(readLine()!.split(separator: " ").map{Int(String($0))!})
 }
 
-var set  = Set(Array(0..<n))
-
-var ans = Int.max
-
-func calc(_ arr1: [Int], _ arr2: [Int]) -> Int {
-    
-    var sum1 = 0
-    var sum2 = 0
-    
-    for i in 0..<arr1.count {
-        
-        let n1 = arr1[i]
-        
-        for j in stride(from: i+1, to: arr1.count, by: 1) {
-            
-            let n2 = arr1[j]
-            
-            sum1 += s[n1][n2] + s[n2][n1]
+func dfs(_ depth: Int, _ start: Int){
+    if depth == n / 2{
+        var startTeam = 0
+        var linkTeam = 0
+        for i in 1...n{
+            for j in 1...n {
+                if !visited[i] && !visited[j] {
+                    linkTeam += arr[i][j - 1]
+                }
+                if visited[i] && visited[j] {
+                    startTeam += arr[i][j - 1]
+                }
+            }
         }
-    }
-    
-    for i in 0..<arr2.count {
-        
-        let n1 = arr2[i]
-        
-        for j in stride(from: i+1, to: arr2.count, by: 1) {
-            
-            let n2 = arr2[j]
-            
-            sum2 += s[n1][n2] + s[n2][n1]
-        }
-    }
-    
-    return abs(sum1-sum2)
-}
-
-func dfs(_ index: Int, _ now : [Int] ,_ limitLength: Int) {
-    
-    if now.count == limitLength {
-        
-        let startTeam = now
-        let linkTeam = Array(set.subtracting(Set(now)))
-        
-        ans = min(ans,calc(startTeam, linkTeam))
-        
+        result = min(result, abs(startTeam - linkTeam))
         return
     }
-    
-    for i in index..<n {
-        dfs(i+1, now + [i], limitLength)
+    for i in start...n {
+        if !visited[i] {
+            visited[i] = true
+            dfs(depth + 1, i)
+            visited[i] = false
+        }
     }
-    
+
 }
+dfs(0, 0)
 
-
-for i in 1...n/2 { // 최대 절반까지
-    dfs(0, [], i)
-}
-
-print(ans)
-
-
+print(result)
