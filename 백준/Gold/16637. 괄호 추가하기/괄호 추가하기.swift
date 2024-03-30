5ecbd640a6f8b78607a3541ef8b8dc4efedd83ae
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-03-30 09:11:06 +0900

    [Gold III] Title: 괄호 추가하기, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.swift
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.swift
@@ -1,73 +1,64 @@
 import Foundation
 
-
-enum Oper : Character {
-    
+enum Oper: Character {
     case plus = "+"
-    case minus = "-"
-    case mul = "*"
+    case subtract = "-"
+    case multi = "*"
     
     
-    func calc(_ n1:Int, _ n2: Int) -> Int {
-        
+    func calc(_ n1: Int, _ n2: Int) -> Int {
         
         switch self {
+            case .plus:
+                return n1 + n2
+            case .subtract:
+                return n1 - n2
+            case .multi:
+                return n1 * n2
             
-        case .plus:
-            return n1+n2
-        case .minus:
-            return n1-n2
-        case .mul:
-            return n1*n2
         }
+        
     }
     
 }
 
 let n = Int(readLine()!)!
-
-var result = Int.min
-
 let s = Array(readLine()!)
 
-var a = [1,2,3]
 
-func calc (_ index: Int,_ value: Int) {
+var ans: Int = Int.min
+
+func calc(_ index: Int, _ value: Int) {
     
-    if (index >= n) {
-        
-        result = max(result,value)
+    if index >= n {
+        ans = max(ans,value)
         return
     }
-    
-    let op : Oper = index >= 1 ? Oper(rawValue: s[index-1])!  : .plus
+
+    let op1 = index >= 1 ? Oper(rawValue: s[index-1])! : .plus
     
     let n1 = Int(String(s[index]))!
     
-    calc(index+2, op.calc(value, n1)) // 괄호 씌우지 않고 넘어가는 경우
-    
-    // index+2 = 다음 숫자 index+1은 연사자이므로
+    calc(index+2, op1.calc(value, n1))
     
     
-    if index < n-2 {
-        
-        
-        // 1 + 2 + 3
-        // 1 + (2 + 3)
-        
-        let nextOp = Oper(rawValue: s[index+1])!
+    if n-2 > index {
         
         let n2 = Int(String(s[index+2]))!
         
-        let res1 = nextOp.calc(n1, n2) // (2+3)
-        let res2 = op.calc(value, res1) // 1 + 5
+        let op2 =  Oper(rawValue: s[index+1])!
         
-        calc(index+4, res2)
+        let res1 = op2.calc(n1, n2)
+        let res2 = op1.calc(value, res1)
+        
+        
+        calc(index+4,res2)
         
     }
     
+    
 }
 
-calc(0, 0)
+calc(0,0)
 
-print(result)
+print(ans)
