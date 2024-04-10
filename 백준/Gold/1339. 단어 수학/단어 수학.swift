9d6f542e80fd0e745ec2a8bdc12af54169ddf438
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-04-10 10:01:03 +0900

    [Gold IV] Title: 단어 수학, Time: 12 ms, Memory: 79520 KB -BaekjoonHub

---
+++ 백준/Gold/1339. 단어 수학/단어 수학.swift
@@ -0,0 +1,136 @@
+import Foundation
+
+public struct Heap<T> {
+    // 전체 노드
+    var nodes: [T] = []
+    // 비교 연산자
+    let comparer: (T,T) -> Bool
+
+    var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+
+    // 예를 들어, Heap<Int>(comparer: >=) 는 Min Heap
+    init(_ comparer: @escaping (T,T) -> Bool) {
+        self.comparer = comparer
+    }
+
+    // top 반환
+    func top() -> T? {
+        return nodes.first
+    }
+
+    // 삽입
+    mutating func push(_ element: T) {
+        
+
+        nodes.append(element)
+
+        var index = nodes.count - 1 //마지막 원소 가르킴 (현재 들어간 원소)
+
+        while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) { // 부모와 비교 후 정렬 기준에 맞지 않으면 swap
+
+            let parent:Int = (index-1)/2
+
+            nodes.swapAt(index, parent)
+            index = parent
+        }
+    }
+
+    // 삭제
+    mutating func pop() -> T? {
+        guard !nodes.isEmpty else {
+            return nil
+        }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        }
+
+      
+        nodes.swapAt(0, nodes.count-1) //꺼낼 top을 마지막이랑  swap 후
+        let result = nodes.popLast() // 꺼냄
+
+        var index = 0
+
+        //Heapify
+        while index < nodes.count {
+            let left = index * 2 + 1
+            let right = left + 1
+
+            if right < nodes.count {
+                if comparer(nodes[left], nodes[right]),
+                    !comparer(nodes[right], nodes[index]) { //오른쪽과 index가 순서가 안 맞을 때
+                    nodes.swapAt(right, index)
+                    index = right
+                } else if !comparer(nodes[left], nodes[index]){ // left와 index가 순서가 안 맞을 때
+                    nodes.swapAt(left, index)
+                    index = left
+                } else {
+                    break
+                }
+            } else if left < nodes.count {
+                if !comparer(nodes[left], nodes[index]) {
+                    nodes.swapAt(left, index)
+                    index = left
+                } else {
+                    break
+                }
+            } else {
+                break
+            }
+        }
+
+        return result
+    }
+}
+
+
+
+let n = Int(readLine()!)!
+
+
+var arr: [String] = []
+
+for _ in 0..<n {
+    arr.append(readLine()!)
+}
+
+var alpha = [Int](repeating: 0, count: 26)
+
+
+for s in arr {
+    
+    var size = s.count
+    
+    
+    
+    for c in s {
+        let po = Int(pow(10.0,Float(size-1)))
+        alpha[Int(c.asciiValue!) - 65] += po
+        size -= 1
+    }
+    
+    
+}
+
+alpha.sort(by: >)
+
+var number = 9
+
+var answer: Int = 0
+
+
+for alp in alpha {
+    
+    if alp == 0 {
+        break
+    }
+    
+    answer += alp * number
+    number -= 1
+}
+
+print(answer)
+
+
