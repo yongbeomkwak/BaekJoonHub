f4cc73d73bc645222460655f387f343287e2cf3b
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-04-13 19:53:59 +0900

    [Gold V] Title: 탑, Time: 360 ms, Memory: 116556 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.swift
+++ 백준/Gold/2493. 탑/탑.swift
@@ -1,37 +1,33 @@
 import Foundation
 
+let n = Int(readLine()!)!
+
+var stk = readLine()!.split{$0 == " "}.map{Int($0)!}
+
+var tmp :[(Int,Int)] = []
+
+var height: [Int] = [Int](repeating: 0, count: n)
+
+var index = n-1
 
 
-var n = Int(readLine()!)!
-
-var arr:[Int] = readLine()!.split{$0 == " "}.map{Int(String($0))!}
-
-var index:Int = arr.count
-
-var result:[Int] = [Int](repeating:0,count:n)
-
-var notRecieve:[(Int,Int)] = [] // (신호를 쏜 탑 위치, 높이)
-
-
-while !arr.isEmpty {
-    
-    let k = arr.popLast()!
-
+while !stk.isEmpty {
     
     
-    while !notRecieve.isEmpty && k >= notRecieve[notRecieve.count-1].1 { // 아직 수신하지 않은 값이 있고 , 현재 높이보다 작거나 같으면 , 이번 탑에서 수신가능
-            
-            let last:(Int,Int) = notRecieve.popLast()!
-            result[last.0] = index // 해당 신호를 쏜 탑의 수신 탑은 현재 탑이 된다.
+    let h = stk.popLast()!
+
+    
+    while !tmp.isEmpty && tmp.last!.1 <= h {
+        
+        let l = tmp.popLast()!
+        
+        height[l.0] = index+1
+        
     }
     
+    tmp.append((index, h))
+    
     index -= 1
-        
-    notRecieve.append((index,k))
-    
-    
 }
 
-print(result.map{String($0)}.joined(separator: " "))
-
-
+print(height.map{String($0)}.joined(separator:" "))
