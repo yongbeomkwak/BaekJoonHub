dfca98e467b0f41034e55bdc1e324f1c46e0e933
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-04-19 10:04:37 +0900

    [Gold V] Title: 퇴사 2, Time: 1680 ms, Memory: 130756 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.swift
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.swift
@@ -1,29 +1,25 @@
 let n = Int(readLine()!)!
 
-
-var arr : [(Int,Int)] = [(0,0)]
-
-for _ in 0..<n {
-    
-    let input = readLine()!.split{ $0 == " " }.map{Int($0)!}
-    
-    arr.append((input[0],input[1]))
-}
+var task: [(Int,Int)] = [(0,0)]
 
 var dp: [Int] = [Int](repeating: 0, count: n+1)
 
+for _ in 0..<n {
+    let input = readLine()!.split{$0 == " "}.map{Int($0)!}
+    
+    task.append((input[0],input[1]))
+}
 
 for i in 1...n {
     
-    dp[i] = max(dp[i],dp[i-1]) // 바로 전날과 현재 비교 한 후
+    dp[i] = max(dp[i-1],dp[i])
     
-    let finDate = i+arr[i].0-1 // 현재 날짜 + 걸리는시간 -1(오늘제외) = 다음 일 가능한 시간
+    let endDate = i + task[i].0-1
     
-    if finDate <= n { // bottom - top
-        dp[finDate] = max(dp[finDate], dp[i-1]+arr[i].1)
+    if endDate <= n {
+        dp[endDate] = max(dp[endDate],dp[i-1]+task[i].1)
     }
     
 }
-
 
 print(dp[n])
