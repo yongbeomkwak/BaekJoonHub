7af8e9e6529e0d292fd812a4f280be32c9230270
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-04-28 12:18:32 +0900

    [level 1] Title: K번째수, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.swift
+++ 프로그래머스/1/42748. K번째수/K번째수.swift
@@ -2,22 +2,27 @@ import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
     
-    var answer:[Int] = []
+    var ans: [Int] = []
     
     for command in commands {
-    
-        let i = command[0]-1 //인덱스 기준 
-        let j = command[1]
-        let k = command[2]
         
-        let sliced_array = Array(array[i..<j])
-        let sorted_array = sliced_array.sorted()
+        let i: Int = command[0]-1
+        let j: Int = command[1]
+        let k: Int = command[2]-1
         
-        answer.append(sorted_array[k-1])
+        var slice: [Int] = [array[i]]
         
+        if i < j {
+             slice = Array(array[i..<j])
+        }
+        
+        slice.sort()
+        
+        
+        ans.append(slice[k])
         
     }
     
     
-    return answer
+    return ans
 }
