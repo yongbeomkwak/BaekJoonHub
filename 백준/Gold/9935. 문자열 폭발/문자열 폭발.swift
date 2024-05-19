aaadf171ebf723076f3fbc6a1b411ab2df367395
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-05-19 14:14:03 +0900

    [Gold IV] Title: 문자열 폭발, Time: 156 ms, Memory: 109360 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.swift
@@ -1,43 +1,33 @@
-let NOTFOUND = "FRULA"
+var str = Array(readLine()!.map{String($0)})
+let bomb = Array(readLine()!.map{String($0)})
+var stk: [String] = []
 
-var s = readLine()!.map{String($0)}
-let bomb = readLine()!.map{String($0)}
-
-
-
-var stk :[String] = []
-
-for i in 0..<s.count {
-    stk.append(s[i])
+for i in 0..<str.count {
     
-    if stk.count >= bomb.count { // 폭발가능성 있음
+    stk.append(str[i])
+    
+    if stk.count >= bomb.count {
         
         var haveBomb = true
         
         for j in 0..<bomb.count {
             
-            //  stk = abcd
-            //  bomb = cd
-            // stk.count - bomb.count - j ,4-2 +0 = 2 = stk[2] = c , 4-2+1 = 3 , stk[3] = d
- 
             if stk[stk.count-bomb.count+j] != bomb[j] {
                 haveBomb = false
                 break
             }
             
         }
-        
-        if haveBomb { // 뒤에 문자열 폭발
-            
-            for _ in 0..<bomb.count {
-                stk.popLast()!
-            }
-            
+    
+        if haveBomb {
+            for _ in 0..<bomb.count { stk.popLast() }
         }
         
     }
     
 }
+
+let NOTFOUND = "FRULA"
 
 if stk.isEmpty {
     print(NOTFOUND)
