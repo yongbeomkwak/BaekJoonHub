518c571f90c58221c645785222618e68909d0f8f
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-05-20 09:38:51 +0900

    [Silver II] Title: 오리, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/12933. 오리/오리.swift
@@ -0,0 +1,50 @@
+var s = Array(readLine()!)
+
+var result: Int = 0
+var visisted: [Bool] = [Bool](repeating: false, count: s.count)
+var target: [Character] = ["q","u","a","c","k"]
+
+// qu q a c u k q a u a c k c k
+
+
+
+func find(begin:Int) {
+    
+    
+    var i: Int = 0
+    var isSameDuck: Bool = false
+    
+    for j in begin..<s.count {
+        
+        if target[i] == s[j] && !visisted[j] { // 방문한 적 없고
+     
+            visisted[j] = true
+            if target[i] == "k" { // 마지막 k 일경우
+                
+                if !isSameDuck {
+                    result += 1
+                    isSameDuck = true // 이후 발견되는 것은 같은 오리로 취급
+                }
+                i = 0 // 타겟인덱스 다시 처음으로
+                continue
+            }
+            i += 1
+        }
+        
+        
+    }
+    
+}
+
+for i in 0..<s.count {
+    if s[i] == "q" && !visisted[i] {
+        find(begin: i)
+    }
+}
+
+
+if !(visisted.filter({$0 == true}).count == s.count) || s.count % 5 != 0 || result == 0  {
+    print(-1)
+} else {
+    print(result)
+}
