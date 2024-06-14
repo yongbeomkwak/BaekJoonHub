05dffaa17b30ffde5ac34befbedd97cd718c417c
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-06-14 15:42:17 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 400.03 ms, Memory: 185 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 뒤에 있는 큰 수 찾기 - 154539 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539?language=swift) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/154539) 
 
 ### 성능 요약
 
-메모리: 185 MB, 시간: 415.33 ms
+메모리: 185 MB, 시간: 400.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 13일 23:12:24
+2024년 06월 14일 15:42:14
 
 ### 문제 설명
 
--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.swift
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.swift
@@ -2,29 +2,33 @@ import Foundation
 
 func solution(_ numbers:[Int]) -> [Int] {
     
-    let n = numbers.count 
-
-    var answer: [Int] = [Int](repeating:-1,count:n)
+    let n = numbers.count
     
     var stk: [Int] = []
     
-    for i in stride(from:n-1,to: -1,by: -1) {
+    var ans: [Int] = [Int](repeating:-1, count:n)
     
+    
+    for i in stride(from: n-1,to: -1,by: -1) {
         
-        while !stk.isEmpty && stk.last! <= numbers[i] {
+        let number = numbers[i]
+        
+        while !stk.isEmpty && numbers[i] >= stk.last! {
             let _ = stk.popLast()
         }
         
         if !stk.isEmpty {
-            
-            answer[i] = stk.last!
+            ans[i] = stk.last! 
         }
         
-        stk.append(numbers[i])
         
         
+        stk.append(number)
     }
     
+
     
-    return answer
+    
+    
+    return ans
 }
