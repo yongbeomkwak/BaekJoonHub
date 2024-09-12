b4e64d66a933588d9a9ab641626324c3f1d1e44d
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-12 23:19:07 +0900

    [level 2] Title: [3차] 방금그곡, Time: 106.33 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.swift
+++ 프로그래머스/2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.swift
@@ -1,84 +1,82 @@
-
-func convert(_ str:String) -> Int {
-    
-    let split = str.split{$0 == ":"}
-    
-    
-    return Int(split[0])!*60 + Int(split[1])!
-}
-
+import Foundation
 
 extension String {
-
-    // var a:String = "123" 
-    // print(a[0]) = "1"
-
-    subscript(_ index: Int) -> String { 
-        return String(self[self.index(self.startIndex, offsetBy: index)])
+    
+    subscript(_ index: Int) -> String {
+        return String(self[self.index(self.startIndex, offsetBy: index)])  
     }
-
-
-    //let str = "abcde"
-    //print(str[0..<4]) // abcd
-
+    
     subscript(_ range: Range<Int>) -> String {
         let fromIndex = self.index(self.startIndex, offsetBy: range.startIndex)
         let toIndex = self.index(self.startIndex,offsetBy: range.endIndex)
         return String(self[fromIndex..<toIndex])
     }
+}
 
+func calcPlayTime(_ startTime: String, _ endTime: String) -> Int {
+
+    let splitTime1 = startTime.components(separatedBy: ":") 
+    let splitTime2 = endTime.components(separatedBy: ":") 
+
+    let startMin = Int(splitTime1[0])!
+    let startSec = Int(splitTime1[1])!
+    let endMin = Int(splitTime2[0])!
+    let endSec = Int(splitTime2[1])!
+
+    return (endMin*60 + endSec ) - (startMin * 60 + startSec) 
+} 
+
+func makeEntireMelody(_ playSec: Int, _ melody: String) -> String {
+
+    var result: [String] = []
+    let count = melody.count
+
+    for i in 0..<playSec {
+        result.append(melody[i%count])
+    }
+
+    return result.joined()
+}
+
+func replaceSharpWithSymbol(_ code: String) -> String {
+    // C#, D, D#, E, F, F#, G, G#, A, A#
+    return code.replacingOccurrences(of: "C#", with: "H")
+                .replacingOccurrences(of: "D#", with: "I")
+                .replacingOccurrences(of: "F#", with: "J")
+                .replacingOccurrences(of: "G#", with: "K")
+                .replacingOccurrences(of: "A#", with: "L")
+                .replacingOccurrences(of: "B#", with: "M")
 }
 
 func solution(_ m:String, _ musicinfos:[String]) -> String {
     
-    var m = m
-    
-    m = m.replacingOccurrences(of:"C#",with:"H")
-        .replacingOccurrences(of:"D#",with:"I")
-        .replacingOccurrences(of:"G#",with:"J")
-        .replacingOccurrences(of:"A#",with:"K")
-        .replacingOccurrences(of:"F#",with:"L")
+    var m = replaceSharpWithSymbol(m)
+    var longestPlayTime: Int = -1
+    var title = "(None)"
+    let count = m.count
     
     
-    
-    let none: String = "(None)"
-    
-    var musicinfos = musicinfos.map({
-    
-        $0.replacingOccurrences(of:"C#",with:"H")
-        .replacingOccurrences(of:"D#",with:"I")
-        .replacingOccurrences(of:"G#",with:"J")
-        .replacingOccurrences(of:"A#",with:"K")
-        .replacingOccurrences(of:"F#",with:"L")
-
-    })
-    
-    // # 붙은거를 다른 문자로 교체
-    
-    var ans: String = none
-    
-    var time: Int = 0
-    
-    for music in musicinfos {
+    for musicinfo in musicinfos {
+        let seperatedArr = musicinfo.components(separatedBy: ",")
+        let startTime = seperatedArr[0]
+        let endTime = seperatedArr[1]
+        let musicTitle = seperatedArr[2]
+        let melody = replaceSharpWithSymbol(seperatedArr[3])
+   
+        // [0] : 시작 시간
+        // [1] : 끝난 시각
+        // [2] : 음악 제목
+        // [3] : 악보정보
         
-        let music = music.split{$0 == ","}.map{String($0)}
+        let playSec = calcPlayTime(startTime, endTime)
+        let entireMelody = makeEntireMelody(playSec, melody)
         
-        let melody = music[3]
-        
-        let playTime = convert(music[1]) - convert(music[0]) // 플레이 시간(분)
-        
-        var play = ""
-        
-        for i in 0..<playTime {
-            let index = i % melody.count
-            play += melody[index]
-        }
-        
-        if play.contains(m) && time < playTime  { // 재생된 결과에 해당 멜로디가 있으면
-            time = playTime
-            ans = music[2]
+        if entireMelody.contains(m) && longestPlayTime < playSec  {
+            title = musicTitle
+            longestPlayTime = playSec  
+             continue   
         }
     }
-    
-    return ans
+
+    return title
 }
