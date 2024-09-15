1059d84f0acb55ed7e31387956820f9d72e84612
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-15 22:08:18 +0900

    [level 2] Title: [3차] n진수 게임, Time: 725.03 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -1,28 +1,38 @@
+import Foundation
+
 extension String {
-
-    // var a:String = "123" 
-    // print(a[0]) = "1"
-
-    subscript(_ index: Int) -> String { 
+    subscript(_ index: Int) -> String {
         return String(self[self.index(self.startIndex, offsetBy: index)])
     }
 }
 
 func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
     
-    var now = 0
+    var numbers: [String] = [] 
     
-    let end = (t-1)*m  + p // 필요한 길이 
+    var now: Int = 0 // 현재 숫자 
     
-    var numbers: [String] = []
+    var count: Int = 0 // 현재 담은 숫자 갯수 
     
-    while numbers.count < end { // 헤당 길이를 채울 때 까지  진법 
-        numbers += String(now, radix: n).compactMap({ String($0).uppercased() })
-        now += 1
-    }  
+    while count < t * m  {
+        let number = String(now, radix:n).uppercased() // 진법 변환된 숫자
+        numbers.append(number) 
+        now += 1 // 다음 숫자 
+        count += number.count // 해당 숫자만큼 길이 증가
+    }
     
+    var ans: [String] = []
     
+    var index: Int = p-1
+    
+    let joinedNumbers = numbers.joined() // 문자열 
+    
+    while ans.count < t {
+        ans.append(joinedNumbers[index])
+        index += m
+    }
 
     
-    return  stride(from: p - 1, to: end, by: m).reduce("", { $0 + numbers[$1] })
+    
+    return ans.joined()
 }
