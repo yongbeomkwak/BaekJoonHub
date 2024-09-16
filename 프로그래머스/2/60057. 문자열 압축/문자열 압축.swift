1efca5b003be5901a169897cde392327a8415a6e
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-16 23:00:55 +0900

    [level 2] Title: 문자열 압축, Time: 201.44 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.swift
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.swift
@@ -17,48 +17,47 @@ func solution(_ s:String) -> Int {
     
     var minLength = s.count 
     
-    for compress in stride(from: 1, through: s.count/2, by:1) {
+    for compress in stride(from: 1, through: s.count/2, by:1) { // 압축 길이는 1 ~ 문자열 길이의 절반까지 
 
-        var count: Int = 1 
+        var count: Int = 1 // 압축 횟수 
         var arr: [String] = []
-        var now: String = s[0..<compress]
-        var lastCompressedIndex: Int = 0
+        var now: String = s[0..<compress] // 현재 기준 문자열 
+        var lastCompressedIndex: Int = 0 // 마지막 압축된 문자열 
         
+        // 압축 길이만큼 자르면서 진행 
         for i in stride(from:compress, through: s.count - compress ,by:compress) {
             
-            let next = s[i..<i+compress]
+            let next = s[i..<i+compress] // 다음 문자열  
             
-            if now == next  {
+            if now == next  { // 현재 문자열과 다음 문자열과 같으면 압축 , count + 1
                 count += 1 
-            } else {
-                if count != 1 {
-                     arr.append("\(count)\(now)")
-                } else {
-                       arr.append(now)
-                }
-                now = next
-                count = 1 
+            } else { // 같지 않으면 현재 문자는 압축 불가
+                // 압축 횟수가 1이면 생략가능
+                let compressedText =  count != 1 ? "\(count)\(now)" : now
+                arr.append(compressedText)
+                now = next // 현재 문자열 다음 문자열로 갱신
+                count = 1  // 압축 횟수도 1로 초기화
             }
             
-            lastCompressedIndex = i + compress
+            lastCompressedIndex = i + compress // 마지막 압축된 인덱스를 계속 추적
         } 
         
         
-        if count != 1 {
-            arr.append("\(count)\(now)")
-        } else {
-            arr.append(now)
-        }
+        // 반복문을 빠져 나온 후 now를 처리 
+        let compressedText =  count != 1 ? "\(count)\(now)" : now
+        arr.append(compressedText)
         
+        // 마지막 압축 인덱스가 전체 문자 길이보다 작다면 
         if lastCompressedIndex < s.count {
+            // 남은 잔여 문자 처리
             arr.append(s[lastCompressedIndex..<s.count])
         }
         
 
+        // 현재 compress를 기준으로 압축된 최종 문자열과 길이 비교 후 갱신
         minLength = min(minLength, arr.joined().count)
         
-        
-       
+    
     }
     
     
