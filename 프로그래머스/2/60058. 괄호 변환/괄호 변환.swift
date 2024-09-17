366a03afff190da100f23634c0e3912ad4fbd2bd
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-17 20:37:15 +0900

    [level 2] Title: 괄호 변환, Time: 64.74 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.swift
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.swift
@@ -1,87 +1,66 @@
 import Foundation
 
-
-func isBalanced(_ s:String) -> Bool {
-    
-    return s.filter{$0 == "("}.count == s.filter{$0 == ")"}.count
-}
-
-func isCollected(_ s:String) -> Bool {
-    
-    if !isBalanced(s) {
-        return false 
-    }
-    
-    var stk: [Character] = []
-    
-    for c in s {
-        if !stk.isEmpty && c == ")"  {
-            stk.removeLast()
-        }
-        else {
-            stk.append(c)
-        }
-    }
-    
-    return stk.isEmpty
-}
-
-
 extension String {
-
-    // var a:String = "123" 
-    // print(a[0]) = "1"
-
-    subscript(_ index: Int) -> String { 
+    subscript(_ index: Int) -> String {
         return String(self[self.index(self.startIndex, offsetBy: index)])
     }
-
-
-    //let str = "abcde"
-    //print(str[0..<4]) // abcd
-
-    subscript(_ range: Range<Int>) -> String {
-        let fromIndex = self.index(self.startIndex, offsetBy: range.startIndex)
-        let toIndex = self.index(self.startIndex,offsetBy: range.endIndex)
-        return String(self[fromIndex..<toIndex])
-    }
-
 }
 
-func solution(_ p:String) -> String {
+func isBalanced(_ w: String) -> Bool {
     
-    if isCollected(p) {
-        return p
-    }
+    if w.isEmpty { return false }
+    return w.filter{$0 == "("}.count == w.filter{$0 == ")"}.count
+}
+
+func isCorrected(_ w: String) -> Bool {
     
-    var u: String = ""
-    var v: String = ""
+    var stack: [String] = []
     
-    var flag: Bool = true 
-    
-    for c in p { // 2 (분리)
-        
-        if flag {
-            u += String(c)
+    for i in 0..<w.count {
+        if w[i] == "(" {
+            stack.append(w[i])
         } else {
-            v += String(c)
-        } 
-        
-        if isBalanced(u) { // 균형잡힌 문자열 됬으면 flag 반전
-            flag = false
+            if stack.isEmpty {
+                return false 
+            }
+            let _ = stack.popLast()
         }
     }
     
-    if isCollected(u) { //3 
-        return u + solution(v) // 3-1
+    return stack.isEmpty
+}
+
+func reversed(_ u: String) -> String {
+    
+    var u = u
+    let _ = u.removeFirst()
+    let _ = u.removeLast()
+    
+    return u.map({ $0 == ")" ? "(" : ")" }).joined()
+}
+
+
+
+func solution(_ w:String) -> String {
+    if w.isEmpty || isCorrected(w)  {
+        return w
     }
     
-    u.removeFirst() // 4-4
-    u.removeLast() // 4-4
+    var w = w 
+    var u = ""
+    var v = ""
     
-    u = u.map { // 4-4 (반전)
-       return $0 == ")" ? "(" : ")"
-    }.joined()
+    while !isBalanced(u)  {
+        u += String(w.removeFirst())
+    }
     
-    return "("  + solution(v) + ")" + u
+    v = w
+    
+    if isCorrected(u) {
+        return  u + solution(v)
+    }
+    
+
+    
+    return "(" + solution(v) + ")" + reversed(u)
 }
