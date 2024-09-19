65032edaa6f2d4efc962dff5bad6948e95448a18
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-19 20:52:55 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.30 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.swift
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.swift
@@ -1,156 +1,54 @@
 import Foundation
 
-
-extension String {
-
-    // var a:String = "123" 
-    // print(a[0]) = "1"
-
-    subscript(_ index: Int) -> String { 
-        return String(self[self.index(self.startIndex, offsetBy: index)])
-    }
-
-
-    //let str = "abcde"
-    //print(str[0..<4]) // abcd
-
-    subscript(_ range: Range<Int>) -> String {
-        let fromIndex = self.index(self.startIndex, offsetBy: range.startIndex)
-        let toIndex = self.index(self.startIndex,offsetBy: range.endIndex)
-        return String(self[fromIndex..<toIndex])
-    }
-
-}
-
-struct Deque<T> {
-    private var enqueue: [T]
-    private var dequeue: [T] = []
-    
-    public var count: Int {
-        return enqueue.count + dequeue.count
-    }
-    public var isEmpty: Bool {
-        return enqueue.isEmpty && dequeue.isEmpty
-    }
-    public var first: T? {
-        if dequeue.isEmpty {
-            return enqueue.first
-        }
-        return dequeue.last
-    }
-    
-    init(_ queue: [T]) {
-        enqueue = queue
-    }
-    mutating func pushFirst(_ n: T) {
-        dequeue.append(n)
-    }
-    mutating func pushLast(_ n: T) {
-        enqueue.append(n)
-    }
-    mutating func popFirst() -> T? {
-        if dequeue.isEmpty {
-            dequeue = enqueue.reversed()
-            enqueue.removeAll()
-        }
-        return dequeue.popLast()
-        
-    }
-    mutating func popLast() -> T? {
-        var returnValue: T?
-        if enqueue.isEmpty {
-            dequeue.reverse()
-            returnValue = dequeue.popLast()
-            dequeue.reverse()
-        } else {
-            returnValue = enqueue.popLast()
-        }
-        return returnValue
-    }
-    mutating func removeAll() {
-        enqueue.removeAll()
-        dequeue.removeAll()
-    }
-}
-
-let dx:[Int] = [0,0,1,-1]
-let dy:[Int] = [1,-1,0,0]
-
-func bfs(_ x:Int , _ y:Int,_ s:[String]) -> Bool {
-    
-    var visited:[[Bool]] = Array(repeating:[false,false,false,false,false,false],count:6)
-    var dist:[[Int]] = Array(repeating:[0,0,0,0,0,0],count:6)
-    
-    var q = Deque([(x,y)])
-    visited[x][y] = true
-    
-    var isBreakRule = false
-    
-    while !q.isEmpty {
-        
-        var p = q.popFirst()!
-        
-        let cx:Int = p.0
-        let cy:Int = p.1
-        
-        for i in 0..<4 {
-            let nx = cx + dx[i] , ny = cy + dy[i]
-            
-            if nx < 0 || ny < 0 || nx > 4 || ny > 4 {
-                continue
-            }
-            
-            if s[nx][ny] == "X" || visited[nx][ny] {
-                continue
-            }
-            
-            if s[nx][ny] == "P"{
-                
-                if dist[cx][cy] + 1 <= 2 {
-                    isBreakRule = true 
-                    break
-                }
-                
-            }
-            
-            visited[nx][ny] = true 
-            dist[nx][ny] = dist[cx][cy] + 1
-            q.pushLast((nx,ny))
-            
-        }
-        
-        if isBreakRule {
-            break
-        }
-        
-    } 
-    
-    return isBreakRule
-}
-
 func solution(_ places:[[String]]) -> [Int] {
+        
+    var ans: [Int] = []
     
-    var ans:[Int] = [0,0,0,0,0]
+    var places = places.map{ $0.map{ Array($0) } } // 문자열을 배열로 변경
     
-    for i in 0..<5 {
-        var flag = false    
-        for j in 0..<5 {
-            
-            for k in 0..<5 {
-                 if places[i][j][k] == "P"{ // 사람이 앉아있으면 그 자리를 기준으로 맨해튼 거리 측정
-                     flag = bfs(j,k,places[i])
-                 }
-                if flag {
-                    break
+    for place in places {
+        
+        var tmpPlace = place //
+        
+            func check(_ r: Int, _ c: Int, _ dist: Int) -> Bool {
+                
+                // 규칙을 지킴
+                // 1.범위를 벗어 났거나 
+                // 2. 맨해튼 거리가 2초과
+                // 3. 중간에 파티션 있음
+                
+                if !(0..<5 ~= r) || !(0..<5 ~= c) || dist == 3 || tmpPlace[r][c] == "X" { 
+                    return true 
+                }  else if tmpPlace[r][c] == "P" { return false }  // 맨해튼 거리 2안에 또다른 응시자 있으면 불통 
+                
+                let nextDist = dist+1 
+
+                // 모든 방향에 대하여 판단
+                return check(r-1,c,nextDist) && check(r+1,c,nextDist) && check(r,c-1,nextDist) && check(r,c+1,nextDist)
+
+                
+            }
+        
+        // 규칙을 지켰는 . 지여부 
+        var isAuthorized: Bool = true 
+        
+        for (row, seat) in place.enumerated() {
+            for (col, p) in seat.enumerated() {   
+                if p == "P" { // 응시자 발견 
+                     tmpPlace[row][col] = "-" // 현재 앉은 곳을 하이픈으로 표시 
+                     isAuthorized = check(row,col,0)
                 }
-            }
+                if !isAuthorized { break } // 지키지 않았다면 더이상 순회 붚필요
+            }   
             
-            if  flag {
-                 break
-            }
+            if !isAuthorized { break } // 지키지 않았다면 더이상 순회 붚필요
         }
         
-        ans[i] = flag ? 0 : 1
+        if isAuthorized {
+            ans.append(1)
+        } else {
+            ans.append(0)
+        }
         
     }
     
