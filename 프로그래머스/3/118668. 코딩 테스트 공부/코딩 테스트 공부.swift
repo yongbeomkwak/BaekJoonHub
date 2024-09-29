7f0e673df41f28b8ed05bb99de62947bbdcebb01
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-29 21:51:21 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 146.07 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.swift
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.swift
@@ -1,59 +1,55 @@
 import Foundation
 
 func solution(_ alp:Int, _ cop:Int, _ problems:[[Int]]) -> Int {
-
-    /*
-     알고 또는 코딩력을 높이기 위한 방법은 , 1의 시간을 써서 1을 늘리거나
-     문제를 풀어 reward를 받거나 
-    */
     
-    var maxAlp: Int = 0
-    var maxCop: Int = 0
+    // 모든 문제를 풀어야함 
+    
+    var maxAlp: Int = 0 // 최대 Alp
+    var maxCop: Int = 0 // 최대 Cop
     
     for problem in problems {
         maxAlp = max(maxAlp,problem[0])
         maxCop = max(maxCop,problem[1])
     }
     
-    var alp = min(maxAlp,alp)
+    // 초기 상태 초기화
+    var alp = min(maxAlp,alp) 
     var cop = min(maxCop,cop)
     
-    var dp:[[Int]] = [[Int]](repeating:[Int](repeating:Int.max-1, count: maxCop+1), count: maxAlp+1) // dp[alp][cop] = 현재 alp, cop 일 때 , 소요된 시간  
+    var dp:[[Int]] = [[Int]](repeating:[Int](repeating:1000000, count: maxCop+2), count: maxAlp+2) 
     
-    dp[alp][cop] = 0
+    // dp[alp][cop] =  현재 alp와 cop 도달 시 최소 시간 
+    
+    // 알고력과 코딩력을 올리는 방법은 다음과 같다.
+    // 1. 개인 공불르 통해 알고력 또는 코딩력을 1 증가 시킬 수 있다.
+    // 2. 현재 문제를 풀어 reward로 얻을 수 있다. 
+    
+     dp[alp][cop] = 0
     
     for i in alp...maxAlp {
         for j in cop...maxCop {
-            
-            if i+1 <= maxAlp {
-              dp[i+1][j] = min(dp[i+1][j],dp[i][j]+1) // 알고력+1을 할 때 시간이, 현재에서 1시간 공부할 때랑 최솟값 비교
-            }
-            
-            if j+1 <= maxCop {
-              dp[i][j+1] = min(dp[i][j+1], dp[i][j]+1) // 코딩력+1을 할 때 시간이, 현재에서 1시간 공부할 때랑 최솟값 비교
-            }
-            
+            dp[i+1][j] = min(dp[i+1][j], dp[i][j]+1)
+            dp[i][j+1] = min(dp[i][j+1], dp[i][j]+1)
             
             for problem in problems {
-                
-                let alpReq = problem[0]
-                let copReq = problem[1]
-                let alpRwd = problem[2]
-                let copRwd = problem[3]
+                let aReq = problem[0]
+                let cReq = problem[1]
+                let aRew = problem[2]
+                let cRew = problem[3]
                 let cost = problem[4]
                 
-                if i>=alpReq && j>=copReq { // 풀 수 있다면
-            //풀어서 얻은 내 최종 알고,코딩력이 max_alp,max_cop 보다크면 그대로 max_alp,max_cop 에 저장 
-                    let nextAlp = min(maxAlp,i + alpRwd)
-                    let nextCop = min(maxCop,j + copRwd)
+                if i >= aReq && j >= cReq {
+                    let nextAlp = min(maxAlp,i + aRew)
+                    let nextCop = min(maxCop,j + cRew)
                     
-                dp[nextAlp][nextCop] = min(dp[nextAlp][nextCop],dp[i][j] + cost) // 현재까지 값과 지금 문제를 풀었을 때 중 , 최소시간
-                    
+                    dp[nextAlp][nextCop] = min(dp[nextAlp][nextCop], dp[i][j] + cost)  
                 }
-                
+
             }
+            
         }
     }
     
+
     return dp[maxAlp][maxCop]
 }
