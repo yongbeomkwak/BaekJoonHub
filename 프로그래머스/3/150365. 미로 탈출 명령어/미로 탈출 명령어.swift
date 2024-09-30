2a4d5a8db1d2aca6364ba4d9e188de739b9f2568
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-09-30 20:59:47 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 73.94 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.swift
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.swift
@@ -1,92 +1,97 @@
 import Foundation
 
-enum Dir: String {
-    
-    case left = "l"
-    case right = "r"
-    case up = "u"
-    case down = "d"
-    
-}
+enum Dir : String {
+    case d
+    case l
+    case r
+    case u
 
-func shift(_ dir: Dir) -> (Int,Int) {
     
+    init(n: Int) {
+        
+        switch n {
+            case 0:
+            self = .l
+            case 1:
+            self = .r
+            case 2:
+            self = .u
+        default:
+            self = .d
+        }
+    }
     
-    switch dir {
-        case .left:
-            return (0,-1)
-        
-        case .right:
-            return (0,1)
-        
-        case .up:
-            return (-1,0)
-        
-        case .down:
-            return (1,0)
+    var coord: (Int, Int) {
+        switch self {
+            
+        case .l:
+            return (0, -1)
+        case .r:
+            return (0, 1)
+        case .u:
+            return (-1, 0)
+        case .d:
+            return (1, 0)
+        }
     }
     
 }
 
-func minDis(_ destX: Int, _ currX: Int, _ destY: Int, _ currY: Int) -> Int {
-    
-    
-    return abs(destX-currX) + abs(destY-currY)
-}
+let dirs: [Dir] = [.d, .l, .r, .u]
 
+func minDist(_ x1: Int, _ y1: Int, _ x2: Int,_ y2: Int) -> Int {
+    return abs(x1-x2) + abs(y1-y2)
+} 
 
 func solution(_ n:Int, _ m:Int, _ x:Int, _ y:Int, _ r:Int, _ c:Int, _ k:Int) -> String {
-    
-    var ans = ""
-    
-    let dir:[Dir] = [.down, .left, .right, .up,] // 알파벳 순을 위해
-    
-    /*
-     n = 세로 길이
-     m = 가로 길이 
-     x = 출발 지점 세로 위치 
-     y = 출발 지점 가로 위치
-     r = 탈출 지점 세로 위치
-     c = 탈출 지점 가로 위치
-     k = 제한 거리
-    */
-    
-    
-    func dfs(_ nowX: Int, _ nowY: Int, _ dist:Int, _ s:String) {
-        if  k < (dist + minDis(r,nowX,c,nowY)) { // 남은 횟수안에 도착 X
+    // n = 세로 길이
+    // m = rkfh rlfdl
+    // x = 초기 세로
+    // y = 초기 가로
+    // r = 도착 세로
+    // c = 도착 가로
+    // k = 제한
+
+    var ans: String = ""
+        
+    func dfs(_ x:Int, _ y: Int, _ dist: Int, _ record:[String]) {
+        
+        if  k < (dist + minDist(x,y,r,c)) { // 남은 횟수안에 도착 X
             return 
         }
         
-        if dist == k && ( nowX ==  r && nowY == c ) { 
-            ans = s
+        if dist == k && ( x ==  r && y == c ) { 
+            ans = record.joined()
             return
         }
         
         for i in 0..<4 {
             
-            let nextDir = dir[i]
-            let (dx,dy) = shift(nextDir)
+            let dir = dirs[i]
+            let (dx, dy) = dir.coord
             
-            let nextX = nowX + dx
-            let nextY = nowY + dy
+            let nextX = x + dx
+            let nextY = y + dy
         
             if  !(1...n ~= nextX) || !(1...m ~= nextY) || !ans.isEmpty { 
                 continue
             }
             
             
-            dfs(nextX,nextY,dist+1,s+nextDir.rawValue)
+            dfs(nextX,nextY,dist+1,record + [dir.rawValue])
             
         }
         
+       
     }
     
-    
-    if  (k < minDis(x,r,y,c)) || (k-minDis(x,r,y,c)) % 2 != 0   { // 맨해튼 거리보다 짧거나 , 그 차이가 홀 수이면 도착 불가 
+    if  (k < minDist(x,y,r,c)) || (k-minDist(x,y,r,c)) % 2 != 0   { 
+        // 맨해튼 거리보다 짧거나 , 그 차이가 홀 수이면 도착 불가 
         return "impossible"
     }
     
-    dfs(x,y,0,"")
+    dfs(x,y,0,[])
     
-    return ans.isEmpty ? "impossible" : ans
+    
+    return ans
 }
