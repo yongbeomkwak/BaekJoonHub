5165d2f5eb217883f6c898be6d546ae6a565bd4a
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-10-01 21:34:22 +0900

    [level 2] Title: 귤 고르기, Time: 219.29 ms, Memory: 22.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.swift
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.swift
@@ -1,25 +1,27 @@
 import Foundation
 
-
 func solution(_ k:Int, _ tangerine:[Int]) -> Int {
     
-    var dict: [Int:Int] = [:]
+    // 귤의 크기를 최소화
     
-    for i in tangerine {
-        dict[i,default:0] += 1 
+    var hash: [Int: Int] = [:]
+    
+   for t in tangerine {
+       hash[t, default:0] += 1
+    }
+
+    var ans:Int = 0  
+    var k = k
+    
+    for (size, count) in  hash.sorted(by: { $0.value > $1.value }) {
+        if k <= 0 {
+            break
+        } else {
+            k -= count 
+            ans += 1
+        }
     }
     
-var sorted_list = dict.sorted{$0.value > $1.value}.map{$0.value}
     
-var cnt: Int = 0
-for (index,v) in sorted_list.enumerated() {
-    if cnt < k {
-        cnt += v 
-        continue
-    }
-    
-    return index
-}
-    
-    return dict.count
+    return ans
 }
