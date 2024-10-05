5654ec36ea7da22b9091dcb6b9f5dbdb9836b82f
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-10-05 11:59:44 +0900

    [level 3] Title: 이중우선순위큐, Time: 559.63 ms, Memory: 22.3 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -6,76 +6,77 @@ enum Command : String {
     case delete = "D"
 }
 
-public struct Heap<T> {
-    // 전체 노드
+struct Heap<T> {
     var nodes: [T] = []
-    // 비교 연산자
-    let comparer: (T,T) -> Bool
-
-    var isEmpty: Bool {
-        return nodes.isEmpty
-    }
-
-    // 예를 들어, Heap<Int>(comparer: >=) 는 Min Heap
-    init(_ comparer: @escaping (T,T) -> Bool) {
-        self.comparer = comparer
-    }
-
-    // top 반환
+    
     var top: T? {
         return nodes.first
     }
 
-    // 삽입
-    mutating func push(_ element: T) { 
-        
-
+    var count: Int {
+        return nodes.count
+    }
+    
+    var comparer: (T,T) -> Bool
+    
+    var isEmpty: Bool {
+        return nodes.isEmpty
+    }
+    
+    init(comparer: @escaping (T,T) -> Bool) {
+        self.comparer = comparer
+    }
+    
+    mutating func push(_ element: T) {
         nodes.append(element)
-
-        var index = nodes.count - 1 //마지막 원소 가르킴 (현재 들어간 원소)
-
-        while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) { // 부모와 비교 후 정렬 기준에 맞지 않으면 swap
-
-            let parent:Int = (index-1)/2
-
-            nodes.swapAt(index, parent)
+        
+        var index = nodes.count-1
+        
+        var parent = (index-1) / 2
+        
+        while 0 < index && !comparer(nodes[index], nodes[parent]) {
+            nodes.swapAt(parent, index)
             index = parent
+            parent = (index-1)/2
         }
     }
-
-    // 삭제
+    
     mutating func pop() -> T? {
+        
         guard !nodes.isEmpty else {
             return nil
         }
-
-        if nodes.count == 1 {
+        
+        guard nodes.count > 1 else {
             return nodes.removeLast()
         }
-
-      
-        nodes.swapAt(0, nodes.count-1) //꺼낼 top을 마지막이랑  swap 후 
-        let result = nodes.popLast() // 꺼냄 
-
-        var index = 0
-
-        //Heapify
-        while index < nodes.count {
-            let left = index * 2 + 1
-            let right = left + 1
-
-            if right < nodes.count {
-                if comparer(nodes[left], nodes[right]),
-                    !comparer(nodes[right], nodes[index]) { //오른쪽과 index가 순서가 안 맞을 때 
-                    nodes.swapAt(right, index)
-                    index = right
-                } else if !comparer(nodes[left], nodes[index]){ // left와 index가 순서가 안 맞을 때 
+        
+        nodes.swapAt(0, nodes.count-1)
+        
+        let result = nodes.removeLast()
+        
+        var index: Int = 0
+        
+        let n = nodes.count
+        
+        while index < n  {
+            
+            var left = index * 2 + 1
+            var right = left+1
+            
+            if right < n {
+                
+                if comparer(nodes[left], nodes[right]) && !comparer(nodes[right], nodes[index]) {
+                    nodes.swapAt(index, right)
+                   index = right
+                } else if !comparer(nodes[left], nodes[index]) {
                     nodes.swapAt(left, index)
-                    index = left
+                   index = left
                 } else {
                     break
                 }
-            } else if left < nodes.count {
+                
+            } else if left < n {
                 if !comparer(nodes[left], nodes[index]) {
                     nodes.swapAt(left, index)
                     index = left
@@ -85,8 +86,9 @@ public struct Heap<T> {
             } else {
                 break
             }
+    
         }
-
+    
         return result
     }
 }
