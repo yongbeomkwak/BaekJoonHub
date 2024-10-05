ff01f6c3ea6f4cb363dac1a4b3cf7d8e7028b1cf
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-10-05 11:54:05 +0900

    [level 3] Title: 이중우선순위큐, Time: 605.54 ms, Memory: 22 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -1,57 +1,66 @@
 import Foundation
 
-struct Heap<T> {
-    
+
+enum Command : String {
+    case insert = "I"
+    case delete = "D"
+}
+
+public struct Heap<T> {
+    // 전체 노드
     var nodes: [T] = []
-    
-    var comparer:(T,T) -> Bool
-    
+    // 비교 연산자
+    let comparer: (T,T) -> Bool
+
     var isEmpty: Bool {
-        return nodes.isEmpty 
+        return nodes.isEmpty
     }
-    
-    var count: Int {
-        return nodes.count 
-    }
-    
-    var first: T? {
-        return nodes.first
-    }
-    
+
+    // 예를 들어, Heap<Int>(comparer: >=) 는 Min Heap
     init(_ comparer: @escaping (T,T) -> Bool) {
         self.comparer = comparer
     }
-    
-    mutating func push(_ element: T) {
-    
-        nodes.append(element)
-        
-        var index = nodes.count-1 
-        var parent = (index-1)/2
-        
-        
-        while index > 0 && !comparer(nodes[index],nodes[parent]) {
-            nodes.swapAt(index,parent)
-            index = parent
-            parent = (index-1)/2
-        }
-        
+
+    // top 반환
+    var top: T? {
+        return nodes.first
     }
-    
-    mutating func pop() -> T? {
+
+    // 삽입
+    mutating func push(_ element: T) { 
         
-        if nodes.count <= 1 {
-            return nodes.popLast()
+
+        nodes.append(element)
+
+        var index = nodes.count - 1 //마지막 원소 가르킴 (현재 들어간 원소)
+
+        while index > 0, !comparer(nodes[index],nodes[(index-1)/2]) { // 부모와 비교 후 정렬 기준에 맞지 않으면 swap
+
+            let parent:Int = (index-1)/2
+
+            nodes.swapAt(index, parent)
+            index = parent
         }
-        
-        nodes.swapAt(0,nodes.count-1)
-        
-        let result = nodes.popLast()
-        
-        var index = 0 
-        
-        
-         while index < nodes.count {
+    }
+
+    // 삭제
+    mutating func pop() -> T? {
+        guard !nodes.isEmpty else {
+            return nil
+        }
+
+        if nodes.count == 1 {
+            return nodes.removeLast()
+        }
+
+      
+        nodes.swapAt(0, nodes.count-1) //꺼낼 top을 마지막이랑  swap 후 
+        let result = nodes.popLast() // 꺼냄 
+
+        var index = 0
+
+        //Heapify
+        while index < nodes.count {
             let left = index * 2 + 1
             let right = left + 1
 
@@ -80,73 +89,68 @@ struct Heap<T> {
 
         return result
     }
-        
 }
-    
 
 func solution(_ operations:[String]) -> [Int] {
     
-    var minHeap = Heap<Int>({$0>$1})
-    var maxHeap = Heap<Int>({$0<$1})
+    var oper_list:[Operation] = []
     
-    var existDict: [Int:Bool] = [:] 
+    var minHeap = Heap<Int> { $0 >= $1 }
+    var maxHeap = Heap<Int> { $0 <= $1 }
+    var isExisted: [Int: Int] = [:]
     
-    func clearNotExistValue(_ heap: inout Heap<Int>) {
-        
-         while !heap.isEmpty && existDict[heap.first!,default:false] == false {
-                    
-                let _ = heap.pop()!
-                  
-         }
-        
-    }
+    for oper in operations {
 
-    operations.forEach { s in 
+        let splited_input = oper.split{$0 == " "}.map{String($0)}
+
+        let command: Command = Command(rawValue:splited_input[0])!
+        let number = Int(splited_input[1])!
+       
+        if command == .insert {
+            minHeap.push(number)
+            maxHeap.push(number)
+            isExisted[number, default: 0] += 1 
+        }
         
-        let op = s.split{$0 == " "}.map{String($0)}
-        
-                        
-        let comm = op[0] 
-        let num = Int(op[1])!
-                        
-        if comm == "I" {
+        else {
             
-            minHeap.push(num)
-            maxHeap.push(num)
-            existDict[num] = true 
-            
-        } else {
-            
-            if num < 0 {
-                
-                clearNotExistValue(&minHeap)
-                
-                if !minHeap.isEmpty {
-                    existDict[minHeap.pop()!] = false 
+            if number == 1 {
+                while !maxHeap.isEmpty && isExisted[maxHeap.top!, default:0] == 0 {
+                    let _ = maxHeap.pop()!
                 }
                 
+                if !maxHeap.isEmpty {
+                    let top = maxHeap.pop()!
+                    isExisted[top, default: 0] -= 1 
+                }
                 
                 
             } else {
-                
-                  clearNotExistValue(&maxHeap)
-                
-                if !maxHeap.isEmpty {
-                    existDict[maxHeap.pop()!] = false 
+                while !minHeap.isEmpty && isExisted[minHeap.top!, default:0] == 0 {
+                    let _ = minHeap.pop()!
                 }
                 
+                if !minHeap.isEmpty {
+                    let top = minHeap.pop()!
+                    isExisted[top, default: 0] -= 1 
+                }
             }
             
+          
         }
-                        
-                        
+        
     }
     
-     clearNotExistValue(&minHeap)
-     clearNotExistValue(&maxHeap)
+
+    while !maxHeap.isEmpty && isExisted[maxHeap.top!, default:0] == 0 {
+        let _ = maxHeap.pop()!
+    }
+                
+
+    while !minHeap.isEmpty && isExisted[minHeap.top!, default:0] == 0 {
+        let _ = minHeap.pop()!
+    }
+
     
-    var result = [maxHeap.pop() ?? 0, minHeap.pop() ?? 0]
-    
-    
-    return result
+    return [maxHeap.pop() ?? 0 , minHeap.pop() ?? 0]
 }
