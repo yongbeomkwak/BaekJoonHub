713e3e1f6cd9b79c3c1f5e51efef7583f502c892
Hamp <48616183+yongbeomkwak@users.noreply.github.com>
2024-10-14 22:41:30 +0900

    [Gold IV] Title: 부분합, Time: 36 ms, Memory: 86700 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.swift
+++ 백준/Gold/1806. 부분합/부분합.swift
@@ -1,34 +1,33 @@
+import Foundation
+
 let ns = readLine()!.split{$0 == " "}.map{Int($0)!}
 
-let (n, s) = (ns[0],ns[1])
+let (n,s) = (ns[0], ns[1])
 
-var arr = readLine()!.split{$0 == " "}.map{Int($0)!}
+let arr = readLine()!.split{$0 == " "}.map{Int($0)!}
 
-var l = Int.max
+var dist: Int = Int.max
 
-var left = 0 , right = 0 , sum = 0
+var left: Int = 0
+var right: Int = 0
 
-while left <= right  {
+var sum = 0
+
+while left <= right {
     
     if sum >= s {
-        
-        l = min(l,right-left)
+        dist = min(dist,right-left)
         sum -= arr[left]
-        left += 1 // left를 오른쪽으로 한칸 옮김
-        
-    } else if right == n {
+        left += 1
+    } else if right == n  {
         break
-    } else { // 아직 s보다 작을 때
-        
+    } else {
         sum += arr[right]
         right += 1
-        
     }
-    
 }
 
-print(l == Int.max ? 0 : l)
-    
 
+print(dist == Int.max ? 0 : dist)
 
 
